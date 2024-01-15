632d26b08db1da1e9bc2b10c3ec6eecfea61829a
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-01-15 11:15:03 +0100

    Create Day1.sql

---
+++ Paco Mantell/Week1/Day1.sql
@@ -0,0 +1,7 @@
+SELECT A.customer_id AS CLIENTE,
+SUM(B.price) AS TOTAL
+FROM SQL_EN_LLAMAS.CASE01.SALES A
+JOIN SQL_EN_LLAMAS.CASE01.MENU B
+ON A.product_id=B.product_id
+GROUP BY A.customer_id
+ORDER BY A.customer_id
