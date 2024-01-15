5c91ff0300ab0d25924613366c64bcc5796e284a
lauragodoycivica <156660236+lauragodoycivica@users.noreply.github.com>
2024-01-15 11:06:17 +0100

    Add files via upload

---
+++ Laura Godoy/day_1.sql
@@ -0,0 +1,9 @@
+--Día 1. ¿Cuánto ha gastado en total cada cliente en el restaurante?
+
+select mem."customer_id", sum("price")
+from members mem
+left join sales s
+on mem."customer_id"=s."customer_id"
+left join menu men
+on s."product_id"=men."product_id"
+group by mem."customer_id";
