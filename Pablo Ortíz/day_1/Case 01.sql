3addaa6d643ea7097f2de8391f48f1e3b6c3330e
PabloOrtiz24 <156430376+PabloOrtiz24@users.noreply.github.com>
2024-01-15 11:40:12 +0100

    Update Case 01.sql

--- Pablo Ortíz/day_1/Case 01.sql
+++ Pablo Ortíz/day_1/Case 01.sql
@@ -1,1 +1,7 @@
-
+-- Hago un join con las 3 tablas y calculo la suma del precio del producto agrupada por cada cliente
+Select  c.customer_id, sum(b.price) as "Gasto total" from sales as a
+join menu as b
+on a.product_id=b.product_id
+join members as c
+on a.customer_id=c.customer_id
+group by c.customer_id;
