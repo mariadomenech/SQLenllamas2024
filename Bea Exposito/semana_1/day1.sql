84d833e08d1e02e1f462b76e0e8413bd04afc647
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-01-15 11:57:06 +0100

    Update day1.sql

--- Bea Exposito/semana_1/day1.sql
+++ Bea Exposito/semana_1/day1.sql
@@ -1,6 +1,6 @@
 --¿Cuánto ha gastado en total cada cliente en el restaurante?
 SELECT sales.customer_id, SUM(menu.price) as total_gastado
-FROM case1.sales
-INNER JOIN case1.menu 
+FROM case01.sales
+INNER JOIN case01.menu 
 ON sales.product_id = menu.product_id
 GROUP BY sales.customer_id
