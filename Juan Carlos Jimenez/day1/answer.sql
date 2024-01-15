1840e2367d0c21800da4e546e6c0c3f6a5671d95
juanquijb <134274520+juanquijb@users.noreply.github.com>
2024-01-15 11:15:07 +0100

    Update answer.sql

--- Juan Carlos Jimenez/day1/answer.sql
+++ Juan Carlos Jimenez/day1/answer.sql
@@ -1,7 +1,7 @@
-SELECT M."customer_id",SUM("price") as total_gastado
+SELECT M.customer_id,SUM(price) as total_gastado
 FROM SALES S
 JOIN MEMBERS M
-ON M."customer_id"= S."customer_id"
+ON M.customer_id= S.customer_id
 JOIN MENU ME
-ON S."product_id" = ME."product_id"
+ON S.product_id = ME.product_id
 GROUP BY 1;
