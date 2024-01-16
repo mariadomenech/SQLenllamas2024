bee2e2f02a7983124021c017285e65545e87b992
jorgemartcaam-civica <117274822+jorgemartcaam-civica@users.noreply.github.com>
2024-01-16 09:51:10 +0100

    Update day-2.sql

--- Jorge Martínez/day-2.sql
+++ Jorge Martínez/day-2.sql
@@ -14,3 +14,11 @@ select a.customer_id,
         left join SQL_EN_LLAMAS.CASE01.MENU b
             on b.product_id = c.product_id
         group by a.customer_id;
+
+//mostrando todos los clientes (aprovechando el script del día 1 pero omitiendo el menú)
+select a.customer_id,
+        count(cast(c.order_date as text)) as days
+    from SQL_EN_LLAMAS.CASE01.MEMBERS a
+        left join SQL_EN_LLAMAS.CASE01.SALES c 
+            on a.customer_id = c.customer_id
+        group by a.customer_id;
