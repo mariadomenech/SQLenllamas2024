01fe976197ea4cfd99e845ef6abdb990477ee37a
Gitsqlconcha <156665354+Gitsqlconcha@users.noreply.github.com>
2024-01-16 10:36:25 +0100

    Update semana1_dia1_Gasto por Cliente.sql

--- Concepción Agrafojo/semana1_dia1_Gasto por Cliente.sql
+++ Concepción Agrafojo/semana1_dia1_Gasto por Cliente.sql
@@ -4,4 +4,4 @@ select sales.customer_id CLIENTE, sum(menu.price) GASTO
 from case01.sales
 inner join case01.menu
     on sales.product_id= menu.product_id
-group by sales.customer_id
+group by sales.customer_id;
