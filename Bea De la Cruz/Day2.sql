67af194958993712e1daf5ae63bab433e7dc040e
Beacruzmo <156338870+Beacruzmo@users.noreply.github.com>
2024-01-16 15:34:58 +0100

    Update Day2.sql

--- Bea De la Cruz/Day2.sql
+++ Bea De la Cruz/Day2.sql
@@ -3,6 +3,4 @@ select  members.customer_id
 from    sales
 full outer join members
     on  sales.customer_id = members.customer_id
-left join menu
-    on  sales.product_id = menu.product_id
 group by members.customer_id;
