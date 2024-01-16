f0f12a45c5a1269fbf2461441b44ba866164a08a
juanquijb <134274520+juanquijb@users.noreply.github.com>
2024-01-16 13:20:59 +0100

    Update day2.sql

--- Juan Carlos Jimenez/day2/day2.sql
+++ Juan Carlos Jimenez/day2/day2.sql
@@ -1,3 +1,5 @@
-SELECT customer_id, count(distinct order_date) AS DIAS_VISITADOS
-FROM SALES S
+SELECT M.customer_id, ZEROIFNULL(count(distinct S.order_date)) AS DIAS_VISITADOS
+FROM MEMBERS M
+FULL JOIN SALES S
+ON M.customer_id= S.customer_id
 GROUP BY 1;
