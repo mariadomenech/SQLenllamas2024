ea0fecee2d6ecac86b71e930cfa7d2b4c0cbd76d
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-01-16 12:26:11 +0100

    Delete Bea Exposito/semana_1/day1.sql

--- Bea Exposito/semana_1/day1.sql
+++
@@ -1,6 +0,0 @@
---¿Cuánto ha gastado en total cada cliente en el restaurante?
-SELECT sales.customer_id, SUM(menu.price) as total_gastado
-FROM case01.sales
-INNER JOIN case01.menu 
-ON sales.product_id = menu.product_id
-GROUP BY sales.customer_id
