3d6ae5c1880a9c2a9932e0b083966f0340058b5d
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-17 19:29:59 +0100

    Comentarios Juanpe
    Comentarios Juanpe

--- Manu Reina/SEMANA1/Day1.sql
+++ Manu Reina/SEMANA1/Day1.sql
@@ -8,3 +8,23 @@ INNER JOIN CASE01.MENU
 FULL JOIN CASE01.MEMBERS 
         ON SALES.CUSTOMER_ID = MEMBERS.CUSTOMER_ID
 GROUP BY MEMBERS.CUSTOMER_ID;
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/* 
+El resultado es completamente correcto pero el código tiene dos pequeña deficiencias.
+
+Los FULL JOIN no son muy recomendables, en este caso no altera el resultado y además son pocos registros, pero en tablas
+con miles de registros y en los que no sabes exactamente que tiene cada tabla puede ser un problema. Por ello sería más
+correcto usar un RIGHT, o incluso partir en el FROM de la tabla MEMBERS, hacer LEFT JOIN con SALES, y de nuevo LEFT JOIN
+con MENU.
+
+La comprobación del nulo para poner 0 sería más correcta así: SUM(IFNULL(MENU.PRICE,0)). ¿Por qué?
+ - de tu forma si se diera este caso: 5+null+5 es 0 puesto que 5+null+5 sería null al ser uno de los sumandos null,
+   y por tanto luego el IFNULL lo convierte a 0
+ - de la forma que te cuento: 5+null+5 =10 porque previo a la suma ha cambiado el null por 0.
+
+En cuanto a la limpieza de código nada que objetar, es una opinión subjetiva pero me gusta que esté todo en mayúscula. 
+Respecto a las tabulaciones nada que objetar tampoco, están tal cual las habria hecho yo, que no significa que sea la 
+única forma correcta, pero para mi está TOP!
+*/
