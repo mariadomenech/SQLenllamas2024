d9c3ce93a882c59190852a3097d73c6cf10adf65
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-17 22:10:10 +0100

    Corrección day_2 Laura Godoy

--- Laura Godoy/day_2.sql
+++ Laura Godoy/day_2.sql
@@ -5,3 +5,15 @@ from sales sales
 right join members members
 on members.customer_id=sales.customer_id
 group by members.customer_id;
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+
+Bien planteado, pero, ¿dónde ha quedado la columna  members.customer_id en la select? si yo fuera Josep, me costaría adivinar a qué cliente corresponde cada conteo.
+
+Por último, dale un alias a la columna count(distinct(order_date)) y PERFECTO!
+
+*/
