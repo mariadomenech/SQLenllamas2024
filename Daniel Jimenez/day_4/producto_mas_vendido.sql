0d218a677f911257e057a8b321a9b81b14f74cb1
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-01-18 09:34:51 +0100

    Update producto_mas_vendido.sql
    Añadido dos versiones, una que amplia el rango a 3 y otra mas compleja

--- Daniel Jimenez/day_4/producto_mas_vendido.sql
+++ Daniel Jimenez/day_4/producto_mas_vendido.sql
@@ -8,3 +8,29 @@ ON sales.product_id = menu.product_id
 GROUP BY menu.product_name
 ORDER BY veces_vendidos DESC
 LIMIT 1;
+
+
+//En esta nueva forma ampliamos el límite a 3 para mostrar todos los productos, algo sencillo 
+
+SELECT  menu.product_name as producto_mas_pedido,
+        COUNT(*) as veces_vendidos
+FROM SQL_EN_LLAMAS.CASE01.SALES
+LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU 
+ON sales.product_id = menu.product_id
+GROUP BY menu.product_name
+ORDER BY veces_vendidos DESC
+LIMIT 3;
+
+//Esta ultima forma devuelve el mismo resultado pero la he elaborado usando algunos agregados y una subconsulta y volviendola ligeramente mas compleja 
+
+SELECT producto_mas_pedido, veces_vendidos
+FROM (
+    SELECT  menu.product_name as producto_mas_pedido,
+            COUNT(*) as veces_vendidos,
+            RANK() OVER (ORDER BY COUNT(*) DESC) as ranking
+    FROM SQL_EN_LLAMAS.CASE01.SALES
+    LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU 
+    ON sales.product_id = menu.product_id
+    GROUP BY menu.product_name
+) AS subconsulta
+WHERE ranking = 1;
