a01da449735f48c847dfa98f63dd679646d949ff
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-18 11:22:41 +0100

    Corrección week_1_day2.sql

--- Javier Fernández/week_1_day2.sql
+++ Javier Fernández/week_1_day2.sql
@@ -6,3 +6,16 @@ SELECT M.CUSTOMER_ID, COUNT(DISTINCT S.ORDER_DATE) AS NUM_VISITS
     LEFT JOIN SALES S
         ON M.CUSTOMER_ID = S.CUSTOMER_ID
     GROUP BY M.CUSTOMER_ID;
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/*
+
+Perfecto Javi, resultado correcto, joins bien montados y el hecho de especificar base de datos y esquema es un ejemplo de buenas prácticas.
+
+Me repito quizás en el tema de las tabulaciones, a mí me resulta más fácil leer las columnas tabuladas tras cada ',', es decir, expandiría la lista de columnas a mostrar.
+
+*/
+	
