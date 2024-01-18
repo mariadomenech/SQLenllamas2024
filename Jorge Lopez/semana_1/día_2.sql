77710dc26ac5592d0535245ca2da750c0e12745a
Yorch <62995534+YorchGR@users.noreply.github.com>
2024-01-18 08:14:01 +0100

    Delete Jorge Lopez/semana_1/día_2.sql

--- Jorge Lopez/semana_1/día_2.sql
+++
@@ -1,4 +0,0 @@
-//Día 2: ¿Cuántos días ha visitado el restaurante cada cliente?
-select sales.customer_id as Clientes, count(sales.order_date) as Cantidad_de_dias
-from case01.sales
-group by sales.customer_id;
