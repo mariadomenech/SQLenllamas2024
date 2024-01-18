a37e96624f61b0d67c8cf4a3576fe2905ba0a0d6
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-18 01:46:37 +0100

    Corrección Day02_version_solo_clientes_que_han_visitado.sql

--- Jose Manuel Bermúdez/day02/Day02_version_solo_clientes_que_han_visitado.sql
+++ Jose Manuel Bermúdez/day02/Day02_version_solo_clientes_que_han_visitado.sql
@@ -2,3 +2,13 @@ SELECT mem.customer_id, COUNT(DISTINCT(sal.order_date))
     FROM sales sal
     JOIN members mem ON sal.customer_id = mem.customer_id
     GROUP BY mem.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+
+/*
+
+Dale un alias a la columna COUNT(DISTINCT(sal.order_date)) y PERFECTO!
+
+*/
