bc96dc57ae70fbdfd382fc6c869b0984d6416036
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-01-18 09:16:56 +0100

    Update producto_mas_vendido.sql

--- Daniel Jimenez/day_4/producto_mas_vendido.sql
+++ Daniel Jimenez/day_4/producto_mas_vendido.sql
@@ -1,10 +1,10 @@
 //¿Cual es el producto mas pedido y cuantas veces se ha pedido?
 
-SELECT  menu.product_name ,
-        COUNT(*) as count
+SELECT  menu.product_name as producto_mas_pedido,
+        COUNT(*) as veces_vendidos
 FROM SQL_EN_LLAMAS.CASE01.SALES
 LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU 
 ON sales.product_id = menu.product_id
 GROUP BY menu.product_name
-ORDER BY count DESC
+ORDER BY veces_vendidos DESC
 LIMIT 1;
