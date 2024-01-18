b35c23f71c9fe5df6bc1f36b557517a690023bec
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-18 01:45:45 +0100

    Corrección Day01_version_todos_los_clientes.sql

--- Jose Manuel Bermúdez/day01/Day01_version_todos_los_clientes.sql
+++ Jose Manuel Bermúdez/day01/Day01_version_todos_los_clientes.sql
@@ -3,3 +3,15 @@ SELECT mem.customer_id, COALESCE(sum(men.price), 0)
         LEFT OUTER JOIN sales sal ON mem.customer_id = sal.customer_id
         LEFT OUTER JOIN menu men ON sal.product_id = men.product_id
     GROUP BY mem.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+
+/*
+
+Guay que conozcas la función colaesce como alternativa a un CASE WHEN o un IFNULL.
+Por otro lado, el OUTER aquí es opcional, pero si personalmente lo ves más claro, no hay diferencia.
+Dale un alias a la columna COALESCE(sum(men.price), 0) y PERFECTO!
+
+*/
