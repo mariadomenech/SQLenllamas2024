7ccffe5b84d76be9ea57d3f6b6c415556d4c9686
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-18 09:57:40 +0100

    Corrección day_2.sql

--- Alberto Pietro/day_2.sql
+++ Alberto Pietro/day_2.sql
@@ -21,3 +21,37 @@ A	6
 B	6
 C	3
 D	0
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto, me ha gustado que muestres el output de la query y también que no des por hecho y añadas los use database y schema, ya que usas solo el nombre de las tablas en la select.
+Respecto al count no se cuál es la finalidad del ",0" ya que no es necesario añadir una segunda expresión.
+
+Mejoraría la legibilidad/visibilidad del código, esto es algo mas de gustos personales y hay varias formas de hacer más legible un código. Por mi parte lo formatería de la siguiente forma:
+
+- Comas por delante de las columnas en el select.
+SELECT
+      m.customer_id
+    , COUNT(s.order_date) AS dias_visitados
+FROM members m
+    LEFT JOIN sales s
+        ON m.customer_id = s.customer_id
+GROUP BY m.customer_id
+ORDER BY dias_visitados DESC;
+
+- Comas por detrás de las columnas en el select.
+SELECT
+    m.customer_id,
+    COUNT(s.order_date) AS dias_visitados
+FROM members m
+    LEFT JOIN sales s
+        ON m.customer_id = s.customer_id
+GROUP BY m.customer_id
+ORDER BY dias_visitados DESC;
+
+Lo que si es común es tabular los JOINs y sus ONs correspondientes.
+
+*/
