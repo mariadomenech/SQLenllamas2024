7400541b0e0f8c44653abfd703cc8a614f280ba9
Beacruzmo <156338870+Beacruzmo@users.noreply.github.com>
2024-01-18 09:45:34 +0100

    Add files via upload

---
+++ Bea De la Cruz/Day4.sql
@@ -0,0 +1,9 @@
+
+select  *
+from    (select  b.product_name,count(*) as num_pedidos
+        from    sales a
+        left join menu b
+            on  a.product_id = b.product_id
+        group by b.product_name
+        order by count(*) desc)
+fetch first 1 rows only;
