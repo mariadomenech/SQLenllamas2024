04df0df5a2f8990bb628911a6ae5b322c603887f
PabloOrtiz24 <156430376+PabloOrtiz24@users.noreply.github.com>
2024-01-18 08:18:16 +0100

    Update Case_03.sql

--- Pablo Ortíz/Day_03/Case_03.sql
+++ Pablo Ortíz/Day_03/Case_03.sql
@@ -1,7 +1,8 @@
 -- En este caso he usado la funcion rank para obtener una columna que me de valor 1 cuando la fecha sea mínima, partiendolo por cada cliente.
 -- Luego he generado una subconsulta para poder quedarme con los registros donde la columna hecha con rank valía 1 usando la función having.
 
-select customer_id,order_date,product_name from
+select customer_id,order_date,product_name
+from
 (
 select  customer_id
 ,a.product_id
