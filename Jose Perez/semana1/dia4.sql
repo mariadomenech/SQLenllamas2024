4489a9713fa2420b02bed42bd66ec0a3663a15ce
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2024-01-18 22:38:17 +0100

    día 4 Jose Perez

---
+++ Jose Perez/semana1/dia4.sql
@@ -0,0 +1,25 @@
+--Utilizo rank para quedarme con varios productos en caso de empate
+with number_order as (
+    select 
+        product_id,
+        count(*) as total
+    from 
+        SQL_EN_LLAMAS.CASE01.SALES
+    group by 
+        product_id
+),
+ranking as (
+    select 
+        product_id,
+        total,
+        rank() over (order by total desc) as _rn
+    from number_order
+)
+select 
+    mn.product_name,
+    r.total as total_purchases
+from 
+    ranking as r 
+left join 
+    SQL_EN_LLAMAS.CASE01.MENU as mn on r.product_id = mn.product_id
+where _rn = 1;
