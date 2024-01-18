6467f5c189c964a4989654d9ea34dbef2c3a67eb
Beacruzmo <156338870+Beacruzmo@users.noreply.github.com>
2024-01-18 14:21:54 +0100

    Update Day4.sql

--- Bea De la Cruz/Day4.sql
+++ Bea De la Cruz/Day4.sql
@@ -1,9 +1,8 @@
 
-select  *
-from    (select  b.product_name,count(*) as num_pedidos
-        from    sales a
-        left join menu b
-            on  a.product_id = b.product_id
-        group by b.product_name
-        order by count(*) desc)
+select  b.product_name,count(*) as num_pedidos
+from    sales a
+left join menu b
+    on  a.product_id = b.product_id
+group by b.product_name
+order by count(*) desc
 fetch first 1 rows only;
