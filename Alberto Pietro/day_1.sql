8007175057b78f4cac00800c3e3130fc8bb690a8
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-18 09:45:56 +0100

    Corrección day_1.sql

--- Alberto Pietro/day_1.sql
+++ Alberto Pietro/day_1.sql
@@ -24,3 +24,40 @@ A	76
 B	74
 C	36
 D	0
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto, me ha gustado el ifnull de la suma, mostrar el output de la query y también añadir los use database y schema, ya que usas solo el nombre de las tablas en la select.
+
+Lo único mejorable sería la legibilidad/visibilidad del código, esto es algo mas de gustos personales y hay varias formas de hacer más legible un código. Por mi parte lo formatería de la siguiente forma:
+
+- Comas por delante de las columnas en el select.
+SELECT
+      m.customer_id
+    , SUM(IFNULL(p.price,0)) AS suma_gastos_cliente
+FROM members m
+    LEFT JOIN sales s
+        ON m.customer_id = s.customer_id
+    LEFT JOIN menu p
+        ON s.product_id = p.product_id
+GROUP BY m.customer_id
+ORDER BY suma_gastos_cliente DESC;
+
+- Comas por detrás de las columnas en el select.
+SELECT
+    m.customer_id,
+    SUM(IFNULL(p.price,0)) AS suma_gastos_cliente
+FROM members m
+    LEFT JOIN sales s
+        ON m.customer_id = s.customer_id
+    LEFT JOIN menu p
+        ON s.product_id = p.product_id
+GROUP BY m.customer_id
+ORDER BY suma_gastos_cliente DESC;
+
+Lo que si es común es tabular los JOINs y sus ONs correspondientes
+
+*/
