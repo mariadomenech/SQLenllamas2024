3b021fbd2d250ac98a01c77fe6b30ceba5c0bc1b
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2024-01-18 22:35:22 +0100

    Create dia1.sql
    dia 1 uno jose perez

---
+++ Jose Perez/semana1/dia1.sql
@@ -0,0 +1,11 @@
+select 
+    mb.customer_id, 
+    sum(nvl(mn.price,0)) as total_spent
+from 
+    SQL_EN_LLAMAS.CASE01.MEMBERS as mb
+left join 
+    SQL_EN_LLAMAS.CASE01.SALES as s on s.customer_id = mb.customer_id
+left join 
+    SQL_EN_LLAMAS.CASE01.MENU as mn on mn.product_id = s.product_id
+group by 
+    mb.customer_id;
