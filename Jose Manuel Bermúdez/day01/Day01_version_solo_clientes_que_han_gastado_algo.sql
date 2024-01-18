ed28578877b5a6831e213b8e52fba51b90410d68
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-18 01:39:37 +0100

    Correccion Day01_version_solo_clientes_que_han_gastado_algo.sql

--- Jose Manuel Bermúdez/day01/Day01_version_solo_clientes_que_han_gastado_algo.sql
+++ Jose Manuel Bermúdez/day01/Day01_version_solo_clientes_que_han_gastado_algo.sql
@@ -3,3 +3,13 @@ SELECT mem.customer_id, sum(men.price)
         JOIN sales sal ON mem.customer_id = sal.customer_id
         JOIN menu men ON sal.product_id = men.product_id
     GROUP BY mem.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+
+/*
+
+Dale un alias a la columna SUM(MENU.PRICE)  y PERFECTO!
+
+*/
