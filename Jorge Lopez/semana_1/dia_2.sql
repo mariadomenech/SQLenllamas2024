b1b0b97a0a003251d8b04f391bbf242eafd11e2c
Yorch <62995534+YorchGR@users.noreply.github.com>
2024-01-18 08:15:35 +0100

    Update dia_2.sql

--- Jorge Lopez/semana_1/dia_2.sql
+++ Jorge Lopez/semana_1/dia_2.sql
@@ -1,1 +1,4 @@
-//prueba 1
+//Día 2: ¿Cuántos días ha visitado el restaurante cada cliente?
+select sales.customer_id as Clientes, count(sales.order_date) as Cantidad_de_dias
+from case01.sales
+group by sales.customer_id;
