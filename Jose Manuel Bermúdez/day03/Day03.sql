828a2b53272e7991f5337da131c8ff4482b4c781
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-18 01:53:13 +0100

    Correccion Day03.sql

--- Jose Manuel Bermúdez/day03/Day03.sql
+++ Jose Manuel Bermúdez/day03/Day03.sql
@@ -7,3 +7,16 @@ INNER JOIN
 	ON sal.customer_id = PRIMER_PEDIDO.customer_id AND sal.order_date = PRIMER_PEDIDO.fecha_primer_pedido
 JOIN menu men ON sal.product_id = men.product_id
 ORDER BY sal.order_date, sal.customer_id, men.product_name;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+
+
+Perfecto Jose Manuel!
+
+Quizás como extra, porque lo que pedimos lo cumples, mejoraría la visualización de los resultados finales, de modo que sean más sencillos de interpretar
+para alguien que vea el resultado en forma de tabla. Te invito a explorar la función LISTAGG.
+
+*/
