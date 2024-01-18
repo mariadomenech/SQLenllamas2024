8fb9ef34c904afba80486c65275993db7edb2be8
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2024-01-18 22:36:56 +0100

    dia 3 jose perez

---
+++ Jose Perez/semana1/dia3.sql
@@ -0,0 +1,22 @@
+with sales_ranking as (
+    select 
+        customer_id,
+        product_id,
+        rank() over ( partition by customer_id order by order_date asc) as _rn
+    from SQL_EN_LLAMAS.CASE01.SALES
+    
+)
+select
+    mb.customer_id,
+    array_agg(distinct nvl(mn.product_name, 'No realizo compra')) as products
+from 
+    SQL_EN_LLAMAS.CASE01.MEMBERS as mb
+left join 
+    sales_ranking as sr on mb.customer_id = sr.customer_id
+left join 
+    SQL_EN_LLAMAS.CASE01.MENU as mn on sr.product_id = mn.product_id
+where 
+    sr._rn = 1 or sr._rn is null
+group by 
+    mb.customer_id
+;
