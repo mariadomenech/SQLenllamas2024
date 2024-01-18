eb004a8f3ef9ab18e9f52e97a211a57a21b5cc6d
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-18 01:47:27 +0100

    CorrecciónDay02_version_todos_los_clientes.sql

--- Jose Manuel Bermúdez/day02/Day02_version_todos_los_clientes.sql
+++ Jose Manuel Bermúdez/day02/Day02_version_todos_los_clientes.sql
@@ -2,3 +2,14 @@ SELECT mem.customer_id, COUNT(DISTINCT(sal.order_date))
     FROM members mem
     LEFT OUTER JOIN sales sal ON mem.customer_id = sal.customer_id
     GROUP BY mem.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+
+/*
+
+Por otro lado, el OUTER aquí es opcional, pero si personalmente lo ves más claro, no hay diferencia.
+Dale un alias a la columna COALESCE(sum(men.price), 0) y PERFECTO!
+
+*/
