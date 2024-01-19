03e3a2395bf8e4ff1610b5a950c85fdc81482b3d
josembacivica <156683470+josembacivica@users.noreply.github.com>
2024-01-19 20:41:13 +0100

    Add files via upload

---
+++ Jose Manuel Bermúdez/day05/Day05.sql
@@ -0,0 +1,10 @@
+SELECT mem.customer_id AS "CLIENTE",
+	   COALESCE(SUM(CASE WHEN men.product_id = 1 THEN 2 * 10 * men.price
+		    		ELSE 10 * men.price
+					END)
+				, 0) AS "PUNTOS TOTALES"
+FROM sales sal
+	JOIN menu men ON sal.product_id = men.product_id
+	RIGHT JOIN members mem ON mem.customer_id = sal.customer_id
+GROUP BY mem.customer_id
+ORDER BY mem.customer_id;
