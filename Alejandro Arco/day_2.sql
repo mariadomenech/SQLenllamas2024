ce395ecd5eaac2e6d9a41eaa1de1937b5ee7f7d1
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-19 08:45:17 +0100

    Corrección day_2.sql

--- Alejandro Arco/day_2.sql
+++ Alejandro Arco/day_2.sql
@@ -8,3 +8,22 @@ LEFT JOIN
 ON members.customer_id = sales.customer_id
 GROUP BY
     members.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto.
+
+Lo único mejorable sería la legibilidad/visibilidad del código. Por mi parte lo formatería de la siguiente forma:
+
+SELECT
+      members.customer_id
+    , COUNT(DISTINCT order_date) AS total_dias
+FROM members
+LEFT JOIN sales
+    ON members.customer_id = sales.customer_id
+GROUP BY members.customer_id;
+
+*/
