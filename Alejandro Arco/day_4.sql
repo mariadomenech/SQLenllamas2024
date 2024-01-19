c7fc6e10b405ad98deac11553a9bc3acf744701d
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-19 09:36:37 +0100

    Corrección day_4.sql

--- Alejandro Arco/day_4.sql
+++ Alejandro Arco/day_4.sql
@@ -10,3 +10,24 @@ LEFT JOIN
 ON MENU.PRODUCT_ID = SALES.PRODUCT_ID
 GROUP BY PRODUCT_NAME
 ORDER BY TOTAL_PEDIDOS DESC;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto. El TOP 1 es correcto y esta bien utilizado, pero comentar que tambien es posible utilizar LIMIT 1:
+
+SELECT
+      PRODUCT_NAME
+    , COUNT(PRODUCT_NAME) AS TOTAL_PEDIDOS
+FROM MENU
+LEFT JOIN SALES
+    ON MENU.PRODUCT_ID = SALES.PRODUCT_ID
+GROUP BY PRODUCT_NAME
+ORDER BY TOTAL_PEDIDOS DESC
+LIMIT 1;
+
+Mejoraría la legibilidad/visibilidad del código como he comentado en los ejercicios previos.
+
+*/
