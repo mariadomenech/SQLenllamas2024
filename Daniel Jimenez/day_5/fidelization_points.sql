5547566334ddc2e49ae5e345baee7f3c0718b7aa
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-01-19 11:30:07 +0100

    Update fidelization_points.sql
    He corregido la query para que contemple al cliente D y muestre su sumatoria a 0

--- Daniel Jimenez/day_5/fidelization_points.sql
+++ Daniel Jimenez/day_5/fidelization_points.sql
@@ -1,31 +1,39 @@
 /*Day 5 - Josep quiere fidelizar a los clientes con 10 puntos por cada euro gastado, pero el sushi tendrá un multiplicador x2*/
 SELECT 
-    sales.customer_id as cliente, 
+    members.customer_id as cliente, 
+    NVL(
     SUM(
         CASE 
             WHEN menu.product_name = 'sushi' THEN MENU.price * 10 * 2
             ELSE menu.price * 10
-        END
-    ) AS PuntosTotales
+        END )
+    ,0) AS PuntosTotales
 FROM 
+    SQL_EN_LLAMAS.CASE01.MEMBERS
+LEFT JOIN 
     SQL_EN_LLAMAS.CASE01.SALES
+        ON members.customer_id = sales.customer_id
 LEFT JOIN 
     SQL_EN_LLAMAS.CASE01.MENU
-        ON SALES.product_id = MENU.product_id
-GROUP BY SALES.customer_id
-ORDER BY SALES.customer_id;
+        ON menu.product_id = sales.product_id
+GROUP BY members.customer_id
+ORDER BY members.customer_id;
 
 /*Otra forma, esta vez usando una CTE para tener el cálculo "encapsulado" y poder recurrir a el mas tarde*/
 
 WITH PuntosPorVenta as (
     SELECT 
-        sales.customer_id as cliente, 
+        members.customer_id as cliente, 
         CASE 
-            WHEN menu.product_name = 'sushi' THEN menu.price * 10 * 2
+            WHEN menu.product_name = 'sushi' THEN menu.price * 10 * 2 
+            WHEN menu.product_name IS NULL THEN 0
             ELSE menu.price * 10
         END as Puntos
     FROM 
+        SQL_EN_LLAMAS.CASE01.MEMBERS
+    LEFT JOIN 
         SQL_EN_LLAMAS.CASE01.SALES
+            ON members.customer_id = sales.customer_id
     LEFT JOIN 
         SQL_EN_LLAMAS.CASE01.MENU
             ON sales.product_id = menu.product_id
