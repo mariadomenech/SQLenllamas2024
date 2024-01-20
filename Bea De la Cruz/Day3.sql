522f6523786a212223d1e350806e4f20be1b59f1
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-20 12:28:58 +0100

    Corrección Day3.sql

--- Bea De la Cruz/Day3.sql
+++ Bea De la Cruz/Day3.sql
@@ -35,3 +35,33 @@ from    (select  distinct a.customer_id
             on b.product_id = c.product_id)
 group by customer_id,join_date
 order by customer_id
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+Quitaría la fecha del resultado final ya que no se pide en el ejercicio. Me ha gustado mucho la utilización de COALESCE y de LISTAGG para hacer mas visible la salida de la query!
+Otra forma posible de hacer el ejercicio es mediante la funcion de ventana RANK, échale un vistazo.
+
+Mejoraría la legibilidad/visibilidad del código un poco.
+
+select
+      customer_id
+    , coalesce(nullif(listagg(product_name, ', ') within group (order by customer_id,join_date),''),'sin pedido') as primer_producto_pedido --por si el cliente no entiende el null
+from (
+    select distinct
+          a.customer_id
+        , a.join_date
+        , c.product_name
+    from members a
+    left join sales b
+        on a.customer_id = b.customer_id
+        and a.join_date = b.order_date
+    left join menu c
+        on b.product_id = c.product_id
+    )
+group by customer_id,join_date
+order by customer_id;
+
+*/
