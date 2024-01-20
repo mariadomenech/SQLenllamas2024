e54c8b9fd6f5e67a579fca44c5c4065e3f193727
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-20 11:35:26 +0100

    Corrección day_5.sql

--- Alejandro Arco/day_5.sql
+++ Alejandro Arco/day_5.sql
@@ -22,3 +22,33 @@ LEFT JOIN SALES
 LEFT JOIN MENU
     ON SALES.PRODUCT_ID = MENU.PRODUCT_ID
 GROUP BY MEMBERS.CUSTOMER_ID;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto y la legibilidad del código ha mejorado. Me ha gustado que no lo des por hecho y añadas los use de la database y el schema a pesar de no ser algo global de SQL sino de Snowflake.
+
+A modo de detalle, otra posible opción sería utilizar un iff en lugar del case, pero esto implicaría hacer uso de una CTE para poder agrupar únicamente por el id del cliente:
+
+WITH CONTEO_PUNTOS AS (
+    SELECT DISTINCT
+          A.CUSTOMER_ID
+        , IFNULL(IFF(B.PRODUCT_ID = 1, 2*10*SUM(C.PRICE), 10*SUM(C.PRICE)), 0) AS PUNTOS
+    FROM MEMBERS A
+        LEFT JOIN SALES B 
+            ON A.CUSTOMER_ID = B.CUSTOMER_ID
+        LEFT JOIN MENU C
+            ON B.PRODUCT_ID = C.PRODUCT_ID
+    GROUP BY A.CUSTOMER_ID, B.PRODUCT_ID
+)
+
+SELECT
+      CUSTOMER_ID
+    , SUM(PUNTOS) AS PUNTOS_FIDELIDAD
+FROM CONTEO_PUNTOS
+GROUP BY CUSTOMER_ID
+ORDER BY CUSTOMER_ID ASC;
+
+*/
