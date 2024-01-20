7979c6d03a00aca57c8ae8f7127ef5e44c48aeb1
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-20 12:57:39 +0100

    Corrección Day5.sql

--- Bea De la Cruz/Day5.sql
+++ Bea De la Cruz/Day5.sql
@@ -1,4 +1,3 @@
-
 select  a.customer_id
         ,sum(case   when c.product_name != 'sushi' then c.price * 10
                     when c.product_name = 'sushi' then c.price * 10 *2
@@ -9,3 +8,25 @@ left join sales b
 left join menu c
     on  b.product_id = c.product_id
 group by a.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto. Solo mejoraría un poco la legibilidad/visibilidad del código un poco.
+
+A modo de detalle, otra posible opción sería utilizar un IFF en lugar del CASE (personalmente solo usaria CASE cuando se den varias condiciones o la única condición se algo compleja),
+haciendo asi el código más limpio:
+
+select
+      a.customer_id
+    , ifnull(sum(iff(c.product_name != 'sushi', c.price * 10, c.price * 10 *2)), 0) as puntos_finalizacion
+from members a
+left join sales b
+    on  a.customer_id = b.customer_id
+left join menu c
+    on  b.product_id = c.product_id
+group by a.customer_id;
+
+*/
