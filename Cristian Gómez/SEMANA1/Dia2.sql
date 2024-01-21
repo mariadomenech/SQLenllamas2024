31cd30eeb1edaa86b7950e5e2875c3eca72d8ee9
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 12:50:49 +0100

    Corrección Dia2.sql

--- Cristian Gómez/SEMANA1/Dia2.sql
+++ Cristian Gómez/SEMANA1/Dia2.sql
@@ -5,3 +5,21 @@ FROM SALES S
 FULL JOIN MEMBERS C
        ON S.CUSTOMER_ID = C.CUSTOMER_ID       
 GROUP BY C.CUSTOMER_ID
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto pero el uso FULL JOIN es algo que se debe evitar ya que, en casos de tablas con miles o millones de registros, el rendimineto se ve afectado drasticamente.
+Por ello, utilizaría LEFT o RIGHT JOIN
+
+SELECT 
+    C.CUSTOMER_ID AS CLIENTE,
+    COUNT(DISTINCT ORDER_DATE) AS DIAS_VISITADOS
+FROM SALES S
+RIGHT JOIN MEMBERS C
+       ON S.CUSTOMER_ID = C.CUSTOMER_ID       
+GROUP BY C.CUSTOMER_ID;
+
+*/
