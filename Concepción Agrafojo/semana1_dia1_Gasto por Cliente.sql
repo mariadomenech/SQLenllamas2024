7df5b1f6d2ec03c05fb40d85ac1984ae8703f563
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 12:26:12 +0100

    Corrección semana1_dia1_Gasto por Cliente.sql

--- Concepción Agrafojo/semana1_dia1_Gasto por Cliente.sql
+++ Concepción Agrafojo/semana1_dia1_Gasto por Cliente.sql
@@ -5,3 +5,23 @@ from case01.sales
 inner join case01.menu
     on sales.product_id= menu.product_id
 group by sales.customer_id;
+
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+Al no utilizar la tabla MEMBERS estamos perdiendo al cliente D, por lo que la añadiría con LEFT JOIN para asi poder verlo, a pesar de no tener ningún pedido de dicho cliente.
+
+select
+      members.customer_id as CLIENTE
+    , sum(ifnull(menu.price, 0)) as GASTO
+from case01.members
+    left join case01.sales
+        on members.customer_id = sales.customer_id
+    left join case01.menu
+        on sales.product_id= menu.product_id
+group by members.customer_id;
+
+*/
