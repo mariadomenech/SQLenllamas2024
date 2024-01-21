e0bff7617f498a1ad338306142f89875455aa81a
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 12:11:05 +0100

    Corrección day5.sql

--- Bea Exposito/day5.sql
+++ Bea Exposito/day5.sql
@@ -19,3 +19,25 @@ FROM
     ) A   
 GROUP BY 1
 ORDER BY 2 DESC;
+
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto, aunque se podria simplificar un poco la query.
+Además, se utiliza FULL JOIN el cual se debe de evitar, por lo que se debería de sustituir por LEFT o RIGHT JOIN (LEFT JOIN en este caso).
+
+SELECT 
+      members.customer_id
+    , SUM(IFNULL(IFF(menu.product_name = 'sushi', menu.price * 10 * 2, menu.price * 10), 0)) AS points
+FROM case01.members 
+LEFT JOIN case01.sales 
+    ON members.customer_id = sales.customer_id
+LEFT JOIN case01.menu 
+    ON menu.product_id = sales.product_id
+GROUP BY 1
+ORDER BY 2 DESC;
+
+*/
