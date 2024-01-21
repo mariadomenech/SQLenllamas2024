91df183b334a27ff614c306cbd030649bad2617b
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 13:13:32 +0100

    Corrección days.sql

--- Daniel Jimenez/day_2/days.sql
+++ Daniel Jimenez/day_2/days.sql
@@ -6,3 +6,21 @@ LEFT JOIN    SQL_EN_LLAMAS.CASE01.SALES B
     ON A.customer_id = B.customer_id
 GROUP BY 1
 ORDER BY 2 DESC;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto, bien utilizado el DISTINCT para no contar el mismo día varias veces. Mejoraría un poco la legibilidad del código:
+
+SELECT
+      A.customer_id as id_cliente
+    , COUNT(DISTINCT B.order_date) as dias_totales
+FROM SQL_EN_LLAMAS.CASE01.MEMBERS A
+    LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES B
+        ON A.customer_id = B.customer_id
+GROUP BY 1
+ORDER BY 2 DESC;
+
+*/
