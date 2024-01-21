7f317229e381e6a2d9e7a991d7fb062b434442ee
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 12:35:04 +0100

    Edit corrección Day2.sql

--- Bea De la Cruz/Day2.sql
+++ Bea De la Cruz/Day2.sql
@@ -10,7 +10,7 @@ group by members.customer_id;
 /*********************************************************/
 /*
 
-El resultado es correcto pero la utilización de FULL OUTER JOIN no lo es, en este caso en concreto con pocos registros que cruzar no supone un problema de rendimiento pero en casos con millones de registros el
+El resultado no es del todo correcto, añadiría un DISTINCT al count para no repetir dias. La utilización de FULL OUTER JOIN no lo es, en este caso en concreto con pocos registros que cruzar no supone un problema de rendimiento pero en casos con millones de registros el
 uso de FULL OUTER JOIN lastra el rendimiento de la consulta consumiendo muchos recursos. Por ello, yo utilizaría como tabla principal la tabla de MEMBERS y utilizaría LEFT JOIN.
 
 Mejoraría la legibilidad/visibilidad del código un poco y utilizaría alias siempre que se hagan cruces de tablas, ya que puede darse el caso en que los nombres de las tablas sean largos o poco claros, lo cual
@@ -18,7 +18,7 @@ dificultaría la lectura del código.
 
 select
       a.customer_id
-    , count(b.order_date) as num_visitas
+    , count(distinct b.order_date) as num_visitas
 from members a
 left join sales b
     on  a.customer_id = b.customer_id
