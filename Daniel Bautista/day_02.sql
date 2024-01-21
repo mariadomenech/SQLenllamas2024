4e5d0eed57696a13ef623fbd64a9c549ada61e5f
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 13:07:38 +0100

    Corrección day_02.sql

--- Daniel Bautista/day_02.sql
+++ Daniel Bautista/day_02.sql
@@ -5,3 +5,21 @@ FROM
     SQL_EN_LLAMAS.CASE01.SALES AS a
 GROUP BY 
     a.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado no es del todo correcto, añadiría la tabla MEMBERS para que se muestre el cliente D a pesar de no haber visitado el restaurante aún.
+También mejoraría un poco el formateo de la query para hacerla más legible.
+
+SELECT 
+    a.customer_id AS cliente,
+    COUNT(distinct b.order_date) AS visitas
+FROM SQL_EN_LLAMAS.CASE01.MEMBERS a
+    LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES b
+        ON a.customer_id = b.customer_id
+GROUP BY a.customer_id;
+
+*/
