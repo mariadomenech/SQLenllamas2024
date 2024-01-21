f51fdaa374816316f0e89379df53f22d27c79d6d
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-21 13:10:53 +0100

    Corrección a.sql

--- Daniel Jimenez/day_1/a.sql
+++ Daniel Jimenez/day_1/a.sql
@@ -9,3 +9,25 @@ LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU B
     ON C."product_id" = B."product_id"
 GROUP BY 1  
 ORDER BY 2 DESC;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+La query da error debido al entre comillado de los campos, cambiando eso el resultado es correcto. Mejoraría un poco la legibilidad del código:
+
+SELECT
+      A.customer_id as id_cliente
+    , SUM(IFNULL(B.price, 0)) as precio
+FROM SQL_EN_LLAMAS.CASE01.MEMBERS A
+LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES C
+    ON A.customer_id = C.customer_id
+LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU B
+    ON C.product_id = B.product_id
+GROUP BY 1  
+ORDER BY 2 DESC;
+
+(Las comas por delante de los campos de la select es una preferencia personal)
+
+*/
