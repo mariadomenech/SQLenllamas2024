45399360378ee59665c095256e7f7e18df415e4d
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:02:45 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Sheila Aguado/Week_1/Day_4.sql
+++ Sheila Aguado/Week_1/Day_4.sql
@@ -6,3 +6,12 @@ ON M.PRODUCT_ID = S.PRODUCT_ID
 GROUP BY M.PRODUCT_NAME
 ORDER BY VECES_PEDIDO DESC
 LIMIT 1
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+¡Todo correcto enhorabuena! PERO OJO, usar LIMIT puede que en caso de empate en el producto más repetido solo te saque uno en vez de tantos como obtengas el primer 
+puesto. Lo mejor hubiera sido una función ventana RANK o tirar de una serie de filtros usando subselect. Si tienes interes en como hacer esto y no te sale no dudes 
+en pregutnar.
+*/
