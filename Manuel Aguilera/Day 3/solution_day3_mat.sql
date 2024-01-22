2387a73bc5c17b0352e206c0c5d6b9015ae7d4e5
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 09:46:36 +0100

    Corrección ejercicio 3
    Corrección ejercicio 3

--- Manuel Aguilera/Day 3/solution_day3_mat.sql
+++ Manuel Aguilera/Day 3/solution_day3_mat.sql
@@ -8,3 +8,13 @@ FROM (
 ) A
 INNER JOIN SQL_EN_LLAMAS.CASE01.MENU B ON (A.PRODUCT_ID=B.PRODUCT_ID)
 WHERE rank_num = 1;
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+Sería interesante mostrar también al cliente que no ha hecho ningun pedido y admeás el cliente C pidio dos platos de ramen y esa información 
+la has perdido con el distinct, el A pidio dos productos en su primer pedido pero al ser distintos no te ha ocurrido esa perdida de información.
+Otra cosa es la visualización de los datos, tal vez con la función LISTAGG sería visualemtne más correcto pues agurparias en una sola fila cada cliente.
+Si no te cuadra lo que te digo no dudes en preguntar.
+*/
