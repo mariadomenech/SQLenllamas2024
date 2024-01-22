899524a199086c025a640532f4cb26727b2f3e3b
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:03:41 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Samir Vega/Day_4.sql
+++ Samir Vega/Day_4.sql
@@ -8,3 +8,12 @@ INNER JOIN MENU B
     ON A.PRODUCT_ID = B.PRODUCT_ID
 GROUP BY PRODUCT_NAME
 ORDER BY CONTEO_PEDIDOS DESC;
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+¡Todo correcto enhorabuena! PERO OJO, usar TOP puede que en caso de empate en el producto más repetido solo te saque uno en vez de tantos como obtengan el primer 
+puesto. Lo mejor hubiera sido una función ventana RANK o tirar de una serie de filtros usando subselect. Si tienes interes en como hacer esto y no te sale no dudes 
+en pregutnar.
+*/
