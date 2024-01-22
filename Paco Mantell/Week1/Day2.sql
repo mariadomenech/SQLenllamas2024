650506f789232377d71b5cf9605eb8fe99465599
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 08:10:45 +0100

    Corrección ejercicio 2
    Corrección ejercicio 2

--- Paco Mantell/Week1/Day2.sql
+++ Paco Mantell/Week1/Day2.sql
@@ -3,3 +3,12 @@ COUNT(DISTINCT order_date) NUM_VISITAS
 FROM SQL_EN_LLAMAS.CASE01.SALES
 GROUP BY customer_id
 ORDER BY 2 DESC
+
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+Para que estuviera del todo correcto haría falta sacar a todos los clientes de la tabla MEMBERS hayan realizado o no una compra. Por lo demás todo OK.
+*/
+
