5d1fbfe71d04bee8384ff2907706c4b8df9ed7eb
carlotavaldivia <156712826+carlotavaldivia@users.noreply.github.com>
2024-01-22 09:12:01 +0100

    Create day1.sql

---
+++ Carlota Valdivia/Week1/day1.sql
@@ -0,0 +1,13 @@
+------------------------------- DÍA 1 -----------------------------
+--- ¿CUÁNTO HA GASTADO EN TOTAL CADA CLIENTE EN EL RESTAURANTE? ---
+-------------------------------------------------------------------
+
+select
+    m.customer_id, 
+    sum(ifnull(p.price,0)) as total_gastado 
+from SQL_EN_LLAMAS.CASE01.MEMBERS m 
+left join SQL_EN_LLAMAS.CASE01.SALES s
+on m.customer_id = s.customer_id
+left join SQL_EN_LLAMAS.CASE01.MENU p 
+on s.product_id = p.product_id 
+group by m.customer_id;
