d7dc537fc5ce331aa029508e8bde85418836ada5
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:16:07 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Maria Nieves Pérez/Reto_4.sql
+++ Maria Nieves Pérez/Reto_4.sql
@@ -11,3 +11,13 @@ WHERE a.product_id = (
     )
 GROUP BY product_name;
 
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+No es correcto, aunque te ha salido bien el resultado no es cierto que la subselect que usas en el where te de el producto más repetido,
+tu lo que estas sacando es el maximo de los id, al ser el id 3 el ramen te sale el ramen porque 1 y 2 que son los otros id son menores.
+Hay dos formas de hacerlo, una es parecido a como lo haces usando una subselect en el where pero donde te quedas con el ramen porque es el más repetido
+no porque sea el del product_id más alto. La otra opción es la función ventana RANK. El uso de funciones como ROW_NUBMER, LIMIT, TOP, te darían bien el resultado
+pero en caso de empate solo te sacarian uno en vez de todos. Te animo a que repitas el ejercicio. ¡Cualquier cosa que no sepas como no dudes en contactar!
+*/
