305067365606c2f45b19712e699252e3583e0a4f
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 08:44:54 +0100

    Corrección ejercicio 2
    Corrección ejercicio 2

--- Pablo Ortíz/Day_2/Case_02.sql
+++ Pablo Ortíz/Day_2/Case_02.sql
@@ -3,3 +3,12 @@
 -- pero es importante usar DISTINCT ya que en la misma visita puede haber varios pedidos del mismo cliente.
 select customer_id as "Cliente", count(distinct(order_date)) as "Número de visitas" from sales
 group by customer_id;
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+Para estar del todo correcto hace falta traer a todos los clientes de la tabla MEMBERS hayan o no realizado algún pedido.
+
+En cuanto a la limpieza de código, aunque este tema siempre es más subjetivo, es conveniente no poner el from en la misma linea de la select.
+*/
