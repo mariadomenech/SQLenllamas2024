73ff0ca1692fd6cda2ded56b1d7f557b7a6c5232
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 09:24:55 +0100

    Corrección ejercicio 3
    Corrección ejercicio 3

--- Sheila Aguado/Week_1/Day_3.sql
+++ Sheila Aguado/Week_1/Day_3.sql
@@ -12,3 +12,21 @@ JOIN SQL_EN_LLAMAS.CASE01.MENU AS MENU
 ON SALES.PRODUCT_ID = MENU.PRODUCT_ID
 GROUP BY SALES.CUSTOMER_ID, SALES.ORDER_DATE, MENU.PRODUCT_NAME
 ORDER BY CLIENTE
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+El cruce con la tabla MENU es más conveniente un LEFT (pero no obligatorio). 
+
+Lo otro si es importante, en el primer cruce te falta una condicón, pues estás cruzando solo por fecha, pero tu tabla del from con tu tabla del primer JOIN 
+debe cruzar también por cliente, es decir, después del primer ON te faltaría AND SALES.CUSTOMER_ID = MIN_DATE.CLIENTE.
+
+Por último el cliente C pone que pidio ramen, pero realmente pidio dos ramen esa información se ha perdido, igual que el cliente A pidio dos platos 
+solo que en su caso son distintos y no se ha perdido esa información. Además es interesante mostrar los resultados en una sola linea por cliente usando 
+la función LISTAGG en la select, LISTAGG(MENU.PRODUCT_NAME,'-') y por tanto el group by solo SALES.CUSTOMER_ID.
+
+Además también es interesante sacar al cliente D ya que esta en la tabla de members aúnque no haya pedido nada.
+
+Cualquier cosa que no te cuadre de lo que te digo no dudes en preguntarme.
+*/
