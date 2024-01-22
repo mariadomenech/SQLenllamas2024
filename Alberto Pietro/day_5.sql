90e33359649dc9afb01bf1e25c1df6bdc2a323ea
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-22 10:09:00 +0100

    Corrección day_5.sql

--- Alberto Pietro/day_5.sql
+++ Alberto Pietro/day_5.sql
@@ -35,3 +35,24 @@ A	          860
 B	          940
 C	          360
 D	          0
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto. A modo de detalle, se podría simplificar en una solo query sin CTE y se podría sustituir CASE por IFF, personalmente usaría CASE cuando haya un control de casuísticas
+o cuando la condición de la única casuística es compleja:
+
+SELECT
+    m.customer_id AS customer_id,
+    SUM(IFNULL(IFF(p.product_name = 'sushi', 2 * 10 * p.price, 10 * p.price), 0)) AS prices
+FROM members m
+LEFT JOIN sales s
+    ON m.customer_id = s.customer_id
+LEFT JOIN menu p
+    ON s.product_id = p.product_id
+GROUP BY m.customer_id
+ORDER BY m.customer_id
+
+*/
