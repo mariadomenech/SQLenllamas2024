f642ccd743a4e8b86605effecfc5a51760ef7624
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-22 11:17:50 +0100

    Corrección dia_3.sql

--- Jorge Lopez/semana_1/dia_3.sql
+++ Jorge Lopez/semana_1/dia_3.sql
@@ -7,3 +7,19 @@ where sales.order_date = (select min(sales.order_date)
                           from case01.sales)
 group by sales.customer_id, sales.order_date
 order by sales.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+
+El resultado es correcto.
+
+¡¡¿Sabías usar la función LISTAGG?!! Muy bien.
+
+Pero aquí ten cuidado con cómo has montado la subconsulta, porque en este caso, coincide que los 3 clientes pidieron por primera vez en el mismo día, pero 
+y, ¿si no hubiera sido así?
+
+Te propongo que le des una vuelta a cómo montarías esa misma subconsulta, para que coja la fecha mínima dependiendo del cliente.
+
+*/
