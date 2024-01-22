e9dcb9c88a41a15ef9da9fb0f7a70e13be8c7c0a
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:22:32 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Luis Garcia/SQL_Dia_4_Luis_García_Serrano.sql
+++ Luis Garcia/SQL_Dia_4_Luis_García_Serrano.sql
@@ -21,3 +21,12 @@ ON M.PRODUCT_ID=S.PRODUCT_ID
 
 GROUP BY M.PRODUCT_NAME           -- SE AGREGA POR EL DESCRITPIVO
 ORDER BY VECES_REPETIDAS DESC     -- SE ORDENA DESCENDENTEMENTE PARA RESOLVER LA PETICIÓN
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+¡Todo correcto enhorabuena! PERO OJO, usar TOP puede que en caso de empate en el producto más repetido solo te saque uno en vez de tantos como obtengan el primer 
+puesto. Lo mejor hubiera sido una función ventana RANK o tirar de una serie de filtros usando subselect. Si tienes interes en como hacer esto y no te sale no dudes 
+en pregutnar.
+*/
