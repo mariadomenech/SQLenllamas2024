904a2f10f68566f2492919e648fa46c495fc0c93
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:04:55 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Pablo Ramón/S1-Day4.sql
+++ Pablo Ramón/S1-Day4.sql
@@ -9,3 +9,12 @@ LEFT JOIN CASE01.MENU M
     ON S.PRODUCT_ID = M.PRODUCT_ID
 GROUP BY 1
 ORDER BY 2 DESC;
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+¡Todo correcto enhorabuena! PERO OJO, usar TOP puede que en caso de empate en el producto más repetido solo te saque uno en vez de tantos como obtengan el primer 
+puesto. Lo mejor hubiera sido una función ventana RANK o tirar de una serie de filtros usando subselect. Si tienes interes en como hacer esto y no te sale no dudes 
+en pregutnar.
+*/
