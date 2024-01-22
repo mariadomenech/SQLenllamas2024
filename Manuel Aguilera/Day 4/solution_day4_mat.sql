9878a9b26e47693571892ad971c2347e1a8091a4
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:17:34 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Manuel Aguilera/Day 4/solution_day4_mat.sql
+++ Manuel Aguilera/Day 4/solution_day4_mat.sql
@@ -9,3 +9,11 @@ INNER JOIN SQL_EN_LLAMAS.CASE01.MENU B
         ON (A.PRODUCT_ID=B.PRODUCT_ID)
 GROUP BY B.PRODUCT_NAME)
 WHERE ROW_NUM=1;
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+¡Todo correcto enhorabuena! PERO OJO, usar ROW_NUMBER puede que en caso de empate en el producto más repetido solo te saque uno en vez de tantos como obtengan el 
+primer puesto. Lo mejor hubiera sido una función ventana RANK.
+*/
