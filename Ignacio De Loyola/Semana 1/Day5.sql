a3f4523bd3c5dad4eb0e81e5a25b829f61fc3747
ignaciobarloz <157125703+ignaciobarloz@users.noreply.github.com>
2024-01-22 18:33:26 +0100

    Create Day5.sql

---
+++ Ignacio De Loyola/Semana 1/Day5.sql
@@ -0,0 +1,20 @@
+-- Dia 5
+with tmp as(
+select
+    c.customer_id
+    , case
+        when product_name = 'sushi' then price*20
+        else price*10
+    end as puntos
+from sales a
+join menu b
+on a.product_id = b.product_id
+right join members c
+on a.customer_id = c.customer_id
+)
+
+select 'El cliente ' || customer_id || ' recibirá ' || sum(nvl(puntos, 0)) || ' puntos.'
+from tmp
+group by customer_id
+order by customer_id
+;
