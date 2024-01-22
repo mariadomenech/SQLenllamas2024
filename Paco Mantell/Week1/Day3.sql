f7a00acbdb959d0bfad2340c6953ec2566593739
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 09:32:13 +0100

    Corrección ejercicio 3
    Corrección ejercicio 3

--- Paco Mantell/Week1/Day3.sql
+++ Paco Mantell/Week1/Day3.sql
@@ -6,3 +6,13 @@ JOIN SQL_EN_LLAMAS.CASE01.MENU B
 ON A.PRODUCT_ID=B.PRODUCT_ID
 WHERE order_date=(SELECT MIN(order_date)
 FROM SQL_EN_LLAMAS.CASE01.SALES)
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+Piensa que el cliente C pidio dos platos de ramen y esa información la has perdido con el distinct, el A pidio dos productos en su primer pedido 
+pero al ser distintos no te ha ocurrido esa perdida de información.
+Otra cosa es la visualización de los datos, tal vez con la función LISTAGG sería visualemtne más correcto pues agurparias en una sola fila cada cliente.
+Si no te cuadra lo que te digo no dudes en preguntar.
+*/
