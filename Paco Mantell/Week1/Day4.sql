9ba1bbcc7d55ef80462966c652e690f27f632f51
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-22 10:04:31 +0100

    Corrección ejercicio 4
    Corrección ejercicio 4

--- Paco Mantell/Week1/Day4.sql
+++ Paco Mantell/Week1/Day4.sql
@@ -5,3 +5,12 @@ LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES B
 ON A.product_id=B.product_id
 GROUP BY 1
 ORDER BY 2 DESC
+
+/*********************************/
+/***** COMENTARIO JUAN PEDRO *****/
+/*********************************/
+/*
+¡Todo correcto enhorabuena! PERO OJO, usar TOP puede que en caso de empate en el producto más repetido solo te saque uno en vez de tantos como obtengan el primer 
+puesto. Lo mejor hubiera sido una función ventana RANK o tirar de una serie de filtros usando subselect. Si tienes interes en como hacer esto y no te sale no dudes 
+en pregutnar.
+*/
