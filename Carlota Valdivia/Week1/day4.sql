e1642d2329ad9a0afe7095dc95476821a7123a35
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-22 10:42:11 +0100

    Corrección day4.sql

--- Carlota Valdivia/Week1/day4.sql
+++ Carlota Valdivia/Week1/day4.sql
@@ -17,3 +17,22 @@ left join SQL_EN_LLAMAS.CASE01.MENU p
 on n.product_id = p.product_id
 order by product_orders desc
 limit 1;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto! Se podría simplificar la query en una sola sin usar una CTE:
+
+select 
+      p.product_name
+    , count(*) as num_orders
+from SQL_EN_LLAMAS.CASE01.SALES s
+    left join SQL_EN_LLAMAS.CASE01.MENU p
+        on s.product_id = p.product_id
+group by p.product_name
+order by num_orders desc
+limit 1;
+
+*/
