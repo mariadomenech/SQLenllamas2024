a6bd8d93a29245c10c7e42ccf449df5b56c4729a
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-22 09:24:05 +0100

    Corrección day_04.sql

--- Daniel Bautista/day_04.sql
+++ Daniel Bautista/day_04.sql
@@ -12,3 +12,22 @@ GROUP BY
 ORDER BY
     ventas DESC
 LIMIT 1;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto. La única mejora que haría seria a la legibilidad del código.
+
+SELECT
+    b.product_name,
+    COUNT(a.product_id) AS ventas
+FROM SQL_EN_LLAMAS.CASE01.SALES as a
+    INNER JOIN SQL_EN_LLAMAS.CASE01.MENU as b
+        ON a.product_id = b.product_id
+GROUP BY b.product_name
+ORDER BY ventas DESC
+LIMIT 1;
+
+*/
