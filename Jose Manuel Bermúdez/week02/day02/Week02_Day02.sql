e44b0e29dd2c101912688d0a08d51280e28bcca5
josembacivica <156683470+josembacivica@users.noreply.github.com>
2024-01-23 16:35:12 +0100

    Update Week02_Day02.sql

--- Jose Manuel Bermúdez/week02/day02/Week02_Day02.sql
+++ Jose Manuel Bermúdez/week02/day02/Week02_Day02.sql
@@ -1,4 +1,7 @@
-SELECt runner_id AS "Runner",
+USE sql_en_llamas;
+USE SCHEMA case02;
+
+SELECT runner_id AS "Runner",
 		SUM(CASE WHEN regexp_replace(distance, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(distance, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) AS "Distancia Acumulada De Reparto", 
 		SUM(CASE WHEN regexp_replace(distance, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(distance, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) /
 			(SUM(CASE WHEN regexp_replace(duration, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(duration, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) / 60) AS "Velocidad Promedio (Km/h)"
