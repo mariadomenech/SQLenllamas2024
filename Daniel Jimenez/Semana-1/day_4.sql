68c2805a62b5bc5ef6fa6271c7dc6faa750dfc60
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-01-23 10:08:18 +0100

    Create day_4.sql

---
+++ Daniel Jimenez/Semana-1/day_4.sql
@@ -0,0 +1,48 @@
+//¿Cual es el producto mas pedido y cuantas veces se ha pedido?
+
+SELECT  menu.product_name as producto_mas_pedido,
+        COUNT(*) as veces_vendidos
+FROM SQL_EN_LLAMAS.CASE01.SALES
+LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU 
+ON sales.product_id = menu.product_id
+GROUP BY menu.product_name
+ORDER BY veces_vendidos DESC
+LIMIT 1;
+
+
+//En esta nueva forma ampliamos el límite a 3 para mostrar todos los productos, algo sencillo 
+
+SELECT  menu.product_name as producto_mas_pedido,
+        COUNT(*) as veces_vendidos
+FROM SQL_EN_LLAMAS.CASE01.SALES
+LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU 
+ON sales.product_id = menu.product_id
+GROUP BY menu.product_name
+ORDER BY veces_vendidos DESC
+LIMIT 3;
+
+//Esta ultima forma devuelve el mismo resultado pero la he elaborado usando algunos agregados y una subconsulta y volviendola ligeramente mas compleja 
+
+SELECT producto_mas_pedido, 
+        veces_vendidos
+FROM (
+    SELECT  menu.product_name as producto_mas_pedido,
+            COUNT(*) as veces_vendidos,
+            RANK() OVER (ORDER BY COUNT(*) DESC) as ranking
+    FROM SQL_EN_LLAMAS.CASE01.SALES
+    LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU 
+    ON sales.product_id = menu.product_id
+    GROUP BY menu.product_name
+) AS subconsulta
+WHERE ranking = 1;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+Me quedo con la 3ª opción por la utilización de la funcion de ventana RANK. El resultado sería correcto.
+
+Como se comentó por el grupo, no añadáis opciones que no devuelvan lo que se pide en el ejercicio.
+
+*/
