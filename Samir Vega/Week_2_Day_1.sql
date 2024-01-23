ee9af78c74195dbef54d1cb33b3954f7c55b7edd
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-23 02:44:09 +0100

    Corrección Juanpe 2.1
    Corrección Juanpe 2.1

--- Samir Vega/Week_2_Day_1.sql
+++ Samir Vega/Week_2_Day_1.sql
@@ -84,3 +84,38 @@ LEFT JOIN (    SELECT
 ON A.RUNNER_ID = B.RUNNER_ID
 INNER JOIN AUX_2 C
 ON A.RUNNER_ID = C.RUNNER_ID;
+
+/****************************/
+/**** COMENTARIOS JUANPE ****/
+/****************************/
+/* 
+En CUSTOMER_ORDERS_CLEAN, comentarte que, aunque yo soy de usar los CASE WHEN también, 
+te ánimo a conocer la función DECODE, está puede sustituir a los CASE WHEN cuando solo se 
+comprueba los posibles, valores de un solo campo de esta forma: 
+    DECODE(CAMPO,WHEN1,THEN1,WHEN2,THEN2,WHEN3,THEN3,ELSE) 
+    O en nuestro caso: DECODE(EXCLUSIONS,'',NULL,'null',NULL,EXCLUSIONS)
+Esto puede ayudarnos a limpiar el código, pues es una forma de escribir un CASE WHEN en una 
+sola línea, si el WHEN requiere la comprobación conjunta de varios campos ya si que tenemos
+que usar CASE WHEN.
+
+Respecto a la tabla RUNNER_ORDERS_CLEAN para este ejercicio no hace más de lo que has hecho,
+pero el campo DISTANCE y el campo DURATION habrá que limpiarlos también, en futuros ejercicios,
+pues te hará falta extraer el valor numérico, así, si quieres, puedes ir pensando cómo hacerlo.
+En cuanto al campo PICKUP_TIME te recomiendo también convertirlo te cuento una función que puede
+serte útil: TRY_CAST(PICKUP_TIME AS TIMESTAMP), la función CAST es para convertir tipo de datos,
+pero si hay inconsistencia falla, en cambio TRY_CAST es una versión de dicha función que devuelve
+NULL si no puede convertirlo esto quiere decir que si viene algo como 'hola','1234', 'null' u otro
+te los va a devolver NULL, algo útil para limpiar ese campo.
+
+Respecto a AUX_1, bien calculo los pedidos y las pizzas entregadas pero el total de pedidos lo
+podías haber obtenido aquí, si no filtras por cancelados y en lugar de COUNT, usas sum(case ...),
+esto haría que no te hiciera falta el LEFT JOIN de la SELECT final.
+
+Todo esto son solo consejos para simplificar el código, pero si que hay un fallo en AUX_2, pues
+en el FROM de esta tabla te faltaría el filtro de WHERE CANCELLATION IS NULL ya que el porcentaje
+de pizzas modificadas se debe calcular respecto de las PIZZAS_ENTREGADAS, aunque de todas formas 
+no haría falta calcularlo ya que lo tienes en la AUX_1, teniendo esto en cuenta podrías simplificar
+como obtienes la tabla AUX_2.
+
+Como siempre, cualquier duda, no dudes en preguntar.
+*/
