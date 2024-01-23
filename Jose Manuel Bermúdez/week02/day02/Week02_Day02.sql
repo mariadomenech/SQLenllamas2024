966a151c1bd281a0f2472efaf75deb61696dcc6e
josembacivica <156683470+josembacivica@users.noreply.github.com>
2024-01-23 16:34:17 +0100

    Add files via upload

---
+++ Jose Manuel Bermúdez/week02/day02/Week02_Day02.sql
@@ -0,0 +1,6 @@
+SELECt runner_id AS "Runner",
+		SUM(CASE WHEN regexp_replace(distance, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(distance, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) AS "Distancia Acumulada De Reparto", 
+		SUM(CASE WHEN regexp_replace(distance, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(distance, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) /
+			(SUM(CASE WHEN regexp_replace(duration, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(duration, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) / 60) AS "Velocidad Promedio (Km/h)"
+FROM runner_orders
+GROUP BY runner_id;
