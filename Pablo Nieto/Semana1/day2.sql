b251d330e9a7f1ac01a7ce9132f21802742631be
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-24 18:07:25 +0100

    Comentario Juanpe 1.2
    Comentario Juanpe 1.2

--- Pablo Nieto/Semana1/day2.sql
+++ Pablo Nieto/Semana1/day2.sql
@@ -6,3 +6,6 @@ FROM SQL_EN_LLAMAS.CASE01.MEMBERS mb
 LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES s
     ON mb.customer_id = s.customer_id
 GROUP BY mb.customer_id;
+
+/* JUANPE: Todo correcto, resultado, código y limpieza, aunque un pequeño matiz, la función COUNT al contrario que SUM si te devuelvo 0 cuando es null por tanto
+no sería necesario la función NVL*/
