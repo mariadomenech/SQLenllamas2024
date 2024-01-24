efd8bdaa29f3d81f054051fa5f99f13366a11f0d
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-24 19:40:12 +0100

    Corrección Juanpe 1.3
    Corrección Juanpe 1.3

--- Pablo Nieto/Semana1/day3.sql
+++ Pablo Nieto/Semana1/day3.sql
@@ -25,3 +25,18 @@ SELECT * FROM first_product;
 /*No sé cómo mostrar una única fila por usuario (supongo que habría que usar PIVOT) pero entonces, si pienso en un conjunto con muchos datos,
 debo saber el número exacto de productos, ¿no? Si lo supiera, tendría una columna por producto más una al final que se llame "Sin pedidos" o algo
 por el estilo pero, ¿no sería tedioso ir mirando si has pedido o no un producto concreto?*/
+
+
+/*
+JUANPE: Sí un pivot aquí sería muy tedioso pero si que hay una función que te permite agrupar en una única fila, la función LISTAGG.
+
+LISTAGG(nvl(mn.product_name, 'sin pedidos'),', ') y como está es una función de agregación necesitas GROUP BY fv.customer_id.
+
+Esta función te agrupa por los campos que pidas en el GROUP BY. Y la función a parte del campo que queremos que ponga en una lista 
+te permite opcionalmente ponerle un separado, por eso la coma entre comillas también te admite un distinct de esta forma 
+LISTAGG(DISTINCT campo, 'separador') pero en este caso si usas el distinct perderiamos la información de que el cliente C, pidió dos ramen.
+
+Esta era la función que buscabamos en este ejercio, espero que a partir de ahora no se te olvide :)
+
+Adicionalmente los resultados se pueden mostrar de una forma "más rebuscada" si tienes interés en esto no dudes en preguntarme.
+*/
