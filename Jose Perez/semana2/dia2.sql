07be10a08981fedf3d6a18ef4f961048256ef1fa
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2024-01-24 07:56:50 +0100

    Create dia2.sql

---
+++ Jose Perez/semana2/dia2.sql
@@ -0,0 +1,24 @@
+with runner_orders_clean as (
+    select 
+        order_id,
+        runner_id,
+        case 
+            when cancellation not in ('Restaurant Cancellation', 'Customer Cancellation') then null::string
+            else cancellation
+        end as cancellation,
+        decode(pickup_time,'null',null,pickup_time) as pickup_time,
+        decode(distance, 'null',null,trim(replace(distance, 'km', '')))::number(35,2) as distance,
+        decode(duration, 'null',null,trim(regexp_replace(duration,'[A-z]',' ')))::number(35,2) as duration
+        
+        
+    from SQL_EN_LLAMAS.CASE02.RUNNER_ORDERS
+
+)
+select 
+    runner_id,
+    sum(distance),0 as total_distance,
+    (sum(distance)/sum(duration))*60 as speed_kh
+from runner_orders_clean
+group by 
+    runner_id
+;
