21cf3b827a56cee1c2683b00adfe0888d857df25
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-24 20:20:09 +0100

    Comentarios Juanpe 1.4
    Comentarios Juanpe 1.4

--- Pablo Nieto/Semana1/day4.sql
+++ Pablo Nieto/Semana1/day4.sql
@@ -14,3 +14,18 @@ FROM SQL_EN_LLAMAS.CASE01.MENU mn
 LEFT JOIN veces_pedido vp
        ON mn.product_id = vp.product_id
 WHERE vp.num_pedidos = (SELECT MAX(num_pedidos) FROM veces_pedido)
+
+
+/*JUANPE: CORRECTO! una de las formas que habia de hacerlo y que fuese correcto al 100%! Ya que usando un filtro te aseguras que en caso de empate
+siempre te de todos los resultados, un TOP o un LIMIT o un ROW_NUMBER no tienen en cuenta los empates.
+Para que la sepas la otra opción correcta es usar RANK (no sé si conoces las funciones ventana) a mi personalmente me gusta más porque te permite
+un código mucho más corto sobre todo si la usas junto a un QUALIFY que te evita usar subconsultas y con una sola select lo tendrías. Te muestro 
+mi propuesta: (Aprovecho y te muestro otra forma de hacer el cruce en un JOIN por si no lo conocias (hay otras también) está obliga a que los campos
+de cruce se llamen iguales)*/
+    SELECT INITCAP(B.PRODUCT_NAME) AS PRODUCTO
+         , COUNT(A.PRODUCT_ID) AS NUMERO_PEDIDOS
+    FROM SALES A
+    LEFT JOIN MENU B USING (PRODUCT_ID) 
+    GROUP BY B.PRODUCT_NAME
+    QUALIFY RANK() OVER (ORDER BY COUNT(A.PRODUCT_ID) DESC) = 1;
+
