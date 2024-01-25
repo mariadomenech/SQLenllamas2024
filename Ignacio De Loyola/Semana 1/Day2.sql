d5fa3088e1024cce64e72e379ca037df30e93269
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-01-25 09:52:57 +0100

    CORRECCIÓN Day2.sql

--- Ignacio De Loyola/Semana 1/Day2.sql
+++ Ignacio De Loyola/Semana 1/Day2.sql
@@ -6,3 +6,13 @@ right join members b
 on a.customer_id = b.customer_id
 group by b.customer_id
 ;
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+El resultado es completamente correcto, buen uso de la tabla de dimensiones members con el RIGHT JOIN.
+
+Y la visualización de la salida, bastante original. Bien hecho!
+
+*/
