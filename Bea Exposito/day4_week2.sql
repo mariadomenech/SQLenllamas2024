0728c0b62e1ac83fbb6a65ef613f27fbf21f96b1
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-01-25 23:46:50 +0100

    Add files via upload

---
+++ Bea Exposito/day4_week2.sql
@@ -0,0 +1,11 @@
+/*Partiendo de la receta de cada una de las pizzas,
+¿cuántas veces se repite cada una topping/ingrediente?*/
+
+
+SELECT B.topping_name
+       ,count(*)
+FROM case02.pizza_recipes A,
+LATERAL FLATTEN(input=>split(toppings, ', ')) C
+INNER JOIN case02.pizza_toppings B
+       ON C.value::string = B.topping_id
+GROUP BY 1;
