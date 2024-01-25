81181a05796df3ccd550f1126f35e23614747666
MariaNievesPerez <156655412+MariaNievesPerez@users.noreply.github.com>
2024-01-25 15:49:14 +0100

    Update Reto_4.sql

--- Maria Nieves Pérez/Reto_4.sql
+++ Maria Nieves Pérez/Reto_4.sql
@@ -21,3 +21,14 @@ Hay dos formas de hacerlo, una es parecido a como lo haces usando una subselect 
 no porque sea el del product_id más alto. La otra opción es la función ventana RANK. El uso de funciones como ROW_NUBMER, LIMIT, TOP, te darían bien el resultado
 pero en caso de empate solo te sacarian uno en vez de todos. Te animo a que repitas el ejercicio. ¡Cualquier cosa que no sepas como no dudes en contactar!
 */
+
+/*********************************/
+/********** CORRECCIÓN ***********/
+/*********************************/
+    SELECT 
+        TOP 1 product_id,
+        COUNT(product_id) as cantidad
+    FROM SQL_EN_LLAMAS.CASE01.SALES
+    GROUP BY product_id
+    ORDER BY cantidad desc;
+
