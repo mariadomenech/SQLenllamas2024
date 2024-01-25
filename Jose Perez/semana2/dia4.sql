9f44b9480783ba8fdfce3b837bf940cc42bc69c9
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2024-01-25 10:40:23 +0100

    Create dia4.sql

---
+++ Jose Perez/semana2/dia4.sql
@@ -0,0 +1,19 @@
+with tabla_pivoteada as  (
+    select  pizza_id , value as topping_id
+    from 
+        SQL_EN_LLAMAS.CASE02.PIZZA_RECIPES, 
+        LATERAL SPLIT_TO_TABLE(toppings, ',') ORDER BY SEQ, INDEX
+        
+)
+SELECT 
+    pt.topping_id,
+    pt.topping_name,
+    count(t.topping_id) as repetitions
+FROM 
+    SQL_EN_LLAMAS.CASE02.PIZZA_TOPPINGS as pt
+left join 
+    tabla_pivoteada as t on  pt.topping_id = t.topping_id
+group by 
+    pt.topping_id,
+    pt.topping_name
+;
