e6eaff27091d2ce227788f6c5614c238155c3924
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-01-25 13:03:58 +0100

    Create Day4.sql

---
+++ Paco Mantell/Week2/Day4.sql
@@ -0,0 +1,15 @@
+WITH CTE_SPLIT_ING AS (
+    /* Desgloso los ingredientes por pizza y lo reuno en dos columnas*/
+    SELECT pizza_id,
+    CAST(C.value AS INT) topping_id
+    FROM sql_en_llamas.case02.pizza_recipes,
+    LATERAL FLATTEN(input=>SPLIT(toppings, ',')) C
+)
+/* Cuento las veces que aparece cada ingrediente*/
+SELECT topping_name,
+ZEROIFNULL(COUNT(B.topping_id)) times_used
+FROM sql_en_llamas.case02.pizza_toppings A
+RIGHT JOIN CTE_SPLIT_ING B
+    ON A.topping_id=B.topping_id
+GROUP BY 1
+ORDER BY 2 DESC
