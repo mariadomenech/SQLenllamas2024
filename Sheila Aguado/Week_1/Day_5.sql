6543dd13b68898ec2d1209ccaac77f965b05259c
Sheila-Aguado <156430296+Sheila-Aguado@users.noreply.github.com>
2024-01-26 17:41:29 +0100

    Corrección Sheila

--- Sheila Aguado/Week_1/Day_5.sql
+++ Sheila Aguado/Week_1/Day_5.sql
@@ -41,3 +41,18 @@ le has dado 20 puntos por haber comprado sushi pero son 200, porque son 20 por c
 Te animo a que lo reahagas de cualquiera de las dos formas que dices aunque si quieres probar otra prueba a usar tablas temporales con el with, son muy útiles 
 cuando se requieren subconsultas. Cualquier duda de como se hace no dudes en contactarme.
 */
+
+SELECT MB.CUSTOMER_ID AS CLIENTE, ZEROIFNULL(P.PUNTOS) AS PUNTOS
+FROM (
+    SELECT S.CUSTOMER_ID AS CLIENTE,
+           SUM (CASE WHEN M.PRODUCT_NAME = 'sushi'
+                THEN M.PRICE*20
+                ELSE M.PRICE*10
+            END) AS PUNTOS
+    FROM SQL_EN_LLAMAS.CASE01.SALES AS S
+    LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU AS M
+    ON S.PRODUCT_ID = M.PRODUCT_ID
+    GROUP BY S.CUSTOMER_ID
+    ) AS P
+RIGHT JOIN SQL_EN_LLAMAS.CASE01.MEMBERS AS MB
+ON P.CLIENTE = MB.CUSTOMER_ID
