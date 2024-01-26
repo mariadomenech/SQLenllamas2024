b304948eb6698b779c7b677132b5fefb332bdebe
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-01-26 08:07:01 +0100

    Corrección day_4.sql

--- Cristina Lucena/SEMANA1/day_4.sql
+++ Cristina Lucena/SEMANA1/day_4.sql
@@ -4,3 +4,27 @@ INNER JOIN SQL_EN_LLAMAS.CASE01.MENU B
 ON A.PRODUCT_ID = B.PRODUCT_ID
 GROUP BY B.PRODUCT_NAME
 ORDER BY CONTEO DESC;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto! Sería más preciso utilizar RANK para asi, en caso de empate, sacar ambos registros.
+
+with ranking as (
+    select
+          b.product_name
+        , count(a.product_id) as conteo
+        , rank() over (order by conteo desc) as rk
+    from sql_en_llamas.case01.sales a
+        inner join sql_en_llamas.case01.menu b
+            on a.product_id = b.product_id
+    group by b.product_name
+)
+
+select * exclude rk
+from ranking
+where rk = 1;
+
+*/
