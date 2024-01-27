69411bb45eda9bcc15c1630ca902bab474ae7650
Pablo Nieto <153496982+panieto@users.noreply.github.com>
2024-01-27 20:53:39 +0100

    Create day4.sql

---
+++ Pablo Nieto/Semana2/day4.sql
@@ -0,0 +1,15 @@
+--Calculamos la frecuencia con la que aparecen los toppings en las pizzas.
+WITH frecuencia_topping AS (
+    SELECT 
+        TRIM(VALUE) AS topping_id,
+        COUNT(TRIM(VALUE)) AS frecuencia
+    FROM SQL_EN_LLAMAS.CASE02.PIZZA_RECIPES,
+    LATERAL SPLIT_TO_TABLE(toppings, ',')
+    GROUP BY TRIM(VALUE)
+)
+--Mostramos los nombres de los toppings y sus frecuencias, estén o no en la receta de alguna pizza.
+SELECT
+    pt.topping_name,
+    NVL(ft.frecuencia, 0) frecuencia
+FROM SQL_EN_LLAMAS.CASE02.PIZZA_TOPPINGS pt
+LEFT JOIN frecuencia_topping ft USING (topping_id)
