89c895805d4e88a070bf3b78f89330259f0945d1
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-01-29 23:46:27 +0100

    Update day1_week3.sql

--- Bea Exposito/day1_week3.sql
+++ Bea Exposito/day1_week3.sql
@@ -1,24 +1,11 @@
-
-/* ¿En cuantos dias de media se reasignan los clientes a un nodo diferente? */
-
----TABLAS LIMPIAS---
-CREATE OR REPLACE TEMPORARY TABLE ESPECIALIDAD_SQL_BRONZE_DB_BEA.RETO.CUSTOMER_NODES AS 
-SELECT 
-    customer_id
-    ,region_id
-    ,node_id
-    ,start_date
-    ,DECODE(end_date,'9999-12-31',NULL,end_date) AS end_date
-FROM case03.customer_nodes;
-
-WITH Nodes_lead AS (
+WITH nodes_lead AS (
     SELECT 
         node_id
         ,start_date
-        ,end_date
+        ,DECODE(end_date,'9999-12-31',NULL,end_date) AS end_date
         ,LEAD(node_id) OVER (PARTITION BY customer_id, region_id ORDER BY start_date) AS next_node_id
         ,LEAD(start_date) OVER (PARTITION BY customer_id, region_id ORDER BY start_date) AS next_start_date
-    FROM ESPECIALIDAD_SQL_BRONZE_DB_BEA.RETO.CUSTOMER_NODES
+    FROM case03.customer_nodes
 )
 
 SELECT 
@@ -29,9 +16,7 @@ FROM
         node_id
         ,start_date
         ,next_start_date - start_date AS difference_days
-    FROM Nodes_lead
+    FROM nodes_lead
     WHERE end_date IS NOT NULL AND next_node_id != node_id
     ORDER BY 1,2,3
 );
-
- 
