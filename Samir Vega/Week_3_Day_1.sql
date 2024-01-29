9abacd0032c7b3dd197d8dab2ea9b4182fe7fcb1
SamirVegaM <156665595+SamirVegaM@users.noreply.github.com>
2024-01-29 17:21:45 +0100

    Add files via upload

---
+++ Samir Vega/Week_3_Day_1.sql
@@ -0,0 +1,16 @@
+--------------------------------------------------------------DIA_1----------------------------------------------------------
+
+WITH AUX AS (
+    SELECT
+        CASE
+            WHEN END_DATE = TO_DATE('9999-12-31', 'YYYY-MM-DD')
+                THEN NULL
+            WHEN NODE_ID != LEAD(NODE_ID) OVER (ORDER BY CUSTOMER_ID, START_DATE) AND END_DATE != TO_DATE('9999-12-31', 'YYYY-MM-DD')
+                THEN DATEDIFF(DAY, START_DATE, END_DATE)
+            WHEN NODE_ID = LEAD(NODE_ID) OVER (ORDER BY CUSTOMER_ID, START_DATE) AND LEAD(END_DATE) OVER (ORDER BY CUSTOMER_ID, START_DATE) != TO_DATE('9999-12-31', 'YYYY-MM-DD')
+                THEN DATEDIFF(DAY, START_DATE, (LEAD(END_DATE) OVER (ORDER BY CUSTOMER_ID, START_DATE)))
+        END AS DIAS_X_NODO
+    FROM CUSTOMER_NODES)
+SELECT
+    ROUND(AVG(DIAS_X_NODO)) AS DIAS_X_NODO_MEDIA
+FROM AUX;
