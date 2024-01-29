229437b44a2e086ac1ee99e31d976dfc4b5aa78f
Beacruzmo <156338870+Beacruzmo@users.noreply.github.com>
2024-01-29 16:41:54 +0100

    Update Day7.sql

--- Bea De la Cruz/Day7.sql
+++ Bea De la Cruz/Day7.sql
@@ -1,16 +1,24 @@
 select 
-    runner_id,
-    sum(distancia_km) as distancia_acum_km,
-    round(sum(distancia_km)/(sum(duracion_min)/60),2) as vel_promedio_km_h
-from (
-select 
-    runner_id,
-    order_id,
-    distance,
-    duration,
-    iff(distance = 'null',null,REGEXP_REPLACE(distance, '[^0-9.]', '')) AS distancia_km,
-    iff(distance = 'null',null,REGEXP_REPLACE(duration, '[^0-9.]', '')) as duracion_min
-from runner_orders
-order by runner_id
-)
-group by runner_id
+    a.runner_id,
+    ifnull(b.distancia_acum_km,0) as distancia_acum_km,
+    ifnull(b.vel_promedio_km_h,0) as vel_promedio_km_h
+from runners a
+left join (
+    select 
+        runner_id,
+        sum(distancia_km) as distancia_acum_km,
+        round(sum(distancia_km)/(sum(duracion_min)/60),2) as vel_promedio_km_h
+    from (
+        select 
+            runner_id,
+            order_id,
+            distance,
+            duration,
+            iff(distance = 'null',null,REGEXP_REPLACE(distance, '[^0-9.]', '')) AS distancia_km,
+            iff(distance = 'null',null,REGEXP_REPLACE(duration, '[^0-9.]', '')) as duracion_min
+        from runner_orders
+        order by runner_id
+        )
+    group by runner_id
+    ) b
+    on a.runner_id = b.runner_id
