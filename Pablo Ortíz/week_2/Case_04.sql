bbfff55f7979fc9fc3b852ece7b0770e181c84d1
PabloOrtiz24 <156430376+PabloOrtiz24@users.noreply.github.com>
2024-01-30 18:35:24 +0100

    Update Case_04.sql

--- Pablo Ortíz/week_2/Case_04.sql
+++ Pablo Ortíz/week_2/Case_04.sql
@@ -1,7 +1,6 @@
 -- En este caso usamos lateral flaten con split para separar los ingredientes en una columna. Necesitamos usar TRIM para eliminar espacios
 -- ya que si no por ejemplo el ingrediente "4" en una fila salía con un espacio y en otra no, por lo que en el count no se agregaba.
 with ingredientes as 
-
 (
 SELECT pizza_id
      , trim(b.value::string,' ') AS ingrediente
@@ -9,7 +8,9 @@ FROM pizza_recipes,
      LATERAL FLATTEN(input=>split(toppings, ',')) as b
      )
      
-     select ingrediente
+     select topping_name as ingrediente
             ,COUNT(ingrediente) as Veces_Repetido
      from ingredientes
-     group by ingrediente;
+     left join pizza_toppings 
+     on ingredientes.ingrediente=pizza_toppings.topping_id
+     group by 1;
