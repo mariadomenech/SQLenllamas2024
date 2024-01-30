d18c8e926b12d92ef07ce22b777a95b43839b757
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-30 01:30:02 +0100

    Comentarios Juanpe 2.2
    Comentarios Juanpe 2.2

--- Manuel Aguilera/Semana 2/Day 2/solution_w2_day2_mat.sql
+++ Manuel Aguilera/Semana 2/Day 2/solution_w2_day2_mat.sql
@@ -17,3 +17,24 @@ FROM SQL_EN_LLAMAS.CASE02.RUNNERS A
 LEFT JOIN RUNNER_ORDERS_CLEAN B
     ON (A.RUNNER_ID=B.RUNNER_ID)
 GROUP BY 1;
+
+
+
+/*JUANPE: 
+
+Resultado: Incorrecto. El motivo son las mates. La velocidad es distancia entre tiempo y la velocidad media es la media de las velocidades pero esto no coincide
+con la suma de las distancias entre las sumas de los tiempo.
+V_m=(v1+v2+vn)/n = (d1/t1+d2/t2+dn/tn)/n <> d_m/t_m = ((d1+d2+dn)/n)/((t1+t2+tn)/n) =(d1+d2+dn)/(t1+t2+tn)
+No sé si esto que te he puesto es un poco lioso, si tienes interes lo podemos ver más despacio (soy un friki de las mates como puedes comprobar).
+
+Código: Lo correto en el código para el resultado correct hubiera sido avg(DISTANCE_KM/DURATION_MIN)*60. Me gusta mucho el uso del TRY_CAST(REGEXP_REPLACE()) para
+limpiar las columnas.
+Otra versión puede ser con la función substr (para substraer), pero su versión mejorada de expresion regular, que nos permite hacerlo así:
+    TRY_CAST(REGEXP_SUBSTR(DISTANCE, '[0-9]*[.]*[0-9]') AS NUMBER(10, 2))
+Aquí le estamos diciendo que substraiga cualquier conjunto de dígitos entre el 0 y el 9 seguidos o no de un punto y seguidos o no de otro conjunto de dígitos entre 0 y 9.
+
+Legibilidad: Correcto
+
+Extra: Bien en sacar al runner 4 y limpiar los nulos por ceros pero la velocidad mostrada solo con dos decimales hubiera sido otro plus
+
+*/
