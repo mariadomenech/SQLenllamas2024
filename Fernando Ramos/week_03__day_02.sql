971f2772c7509b74294020846c2c1ca02527d214
Fernando <147824261+fernando-cvc@users.noreply.github.com>
2024-01-30 19:26:06 +0100

    Create week_03__day_02.sql

---
+++ Fernando Ramos/week_03__day_02.sql
@@ -0,0 +1,24 @@
+with pivot as (
+select 
+    *
+from SQL_EN_LLAMAS.CASE03.customer_transactions
+    PIVOT (count(txn_type)
+        FOR txn_type IN ('deposit','purchase','withdrawal'))
+            AS A (customer_id, txn_date, txn_amount, deposit, purchase, withdrawall)
+),
+
+cuenta as (
+select
+      customer_id
+    , month(txn_date) as month
+from pivot
+    group by 1,2
+    having (sum(deposit)>1 and sum(purchase)>1) or sum(withdrawall)>1
+)
+
+select
+      month as mes
+    , count (customer_id) as n_clientes
+from cuenta
+    group by month
+    order by mes asc
