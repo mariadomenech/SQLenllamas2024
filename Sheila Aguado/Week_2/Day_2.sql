edeb934fc577cc08d148cadebb6d2accde7bd8c2
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-30 01:45:07 +0100

    Comentarios Juanpe 2.2
    Comentarios Juanpe 2.2

--- Sheila Aguado/Week_2/Day_2.sql
+++ Sheila Aguado/Week_2/Day_2.sql
@@ -28,11 +28,12 @@ Resultado: Correcto
 Código: Correcto. Aunque correcto te quiero comentar unas funciones útiles que puedes usar para la limpieza de los campos DISTANCE y DURATION. Hay unas expresiones que se conocen como
 expresiones regulares empiezan por "REGEXP_" son como una versión más "pro" de funciones como replace, substr, count, instr ... En el caso de querer limpiar el campo con un replace podríamos
 hacer algo así: 
-    TRY_CAST(REGEXP_REPLACE(DISTANCE, '[a-zA-Z]', '') AS NUMBER(10, 2))
-Aquí le estamos diciendo que cualquier letra comprendida entre la A y la Z (minúscula y mayúscula), las reemplaza por ''. 
+    TRY_CAST(REGEXP_REPLACE(DISTANCE, '[^0-9.]', '') AS NUMBER(10, 2))
+Aquí le estamos diciendo que remplazo cualquier caracter menos (^) los digitos del 0 al 9 y el "." y los reemplazmos por nada.
 Otra versión puede ser con la función substr (para substraer), su versión mejorada nos permite:
-    TRY_CAST(REGEXP_SUBSTR(DISTANCE, '[0-9]*[.]*[0-9]') AS NUMBER(10, 2))
-Aquí le estamos diciendo que substraiga cualquier conjunto de dígitos entre el 0 y el 9 seguidos o no de un punto y seguidos o no de otro conjunto de dígitos entre 0 y 9.
+    TRY_CAST(REGEXP_SUBSTR(DURATION, '[0-9.]*') AS NUMBER)
+Aquí le estamos diciendo que substraiga cualquier conjunto de dígitos entre el 0 y el 9 y el punto el * es para que cojo todo lo que le pedimos tantas veces como 
+haga falta.
 La diferencia entre el try_cast y el cast es que si hay un registro que no pueda convertir el cast falla y el try_cast lo convierte a nulo. No es mejor o peor uno u otro es cuestión de gustos
 pues si se usa bien no hay problema. Pero en cuanto a las expresiones regulares si te pueden ofrecer ciertas ventajas.
 
