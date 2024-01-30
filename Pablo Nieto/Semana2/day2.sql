97dd605948b842b57093ac09eb45e0a712b86ee6
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-01-30 01:00:05 +0100

    Comentarios Juanpe 2.2
    Comentarios Juanpe 2.2

--- Pablo Nieto/Semana2/day2.sql
+++ Pablo Nieto/Semana2/day2.sql
@@ -20,3 +20,31 @@ FROM SQL_EN_LLAMAS.CASE02.RUNNERS r
 LEFT JOIN pedidos_limpios pl
        ON r.runner_id = pl.runner_id
 GROUP BY r.runner_id;
+
+
+/*JUANPE: 
+
+Resultado: Incorrecto. El motivo son las mates. La velocidad es distancia entre tiempo y la velocidad media es la media de las velocidades pero esto no coincide
+con la suma de las distancias entre las sumas de los tiempo.
+V_m=(v1+v2+vn)/n = (d1/t1+d2/t2+dn/tn)/n <> d_m/t_m = ((d1+d2+dn)/n)/((t1+t2+tn)/n) =(d1+d2+dn)/(t1+t2+tn)
+No sé si esto que te he puesto es un poco lioso, si tienes interes lo podemos ver más despacio (soy un friki de las mates como puedes comprobar).
+
+Código: Lo correto en el código para el resultado correct hubiera sido avg(clean_distance_km/clean_duration_min)*60 (aunque esto te obliga a filtrar los cancelados en la cte)
+para no tener problemas en la división.
+Además te quiero comentar unas funciones útiles que puedes usar para la limpieza de los campos DISTANCE y DURATION. Hay unas expresiones que se conocen como
+expresiones regulares empiezan por "REGEXP_" son como una versión más "pro" de funciones como replace, substr, count, instr ... En el caso de querer limpiar el 
+campo con un replace podríamos hacer algo así: 
+    TRY_CAST(REGEXP_REPLACE(DISTANCE, '[a-zA-Z]', '') AS NUMBER(10, 2))
+Aquí le estamos diciendo que cualquier letra comprendida entre la A y la Z (minúscula y mayúscula), las reemplaza por '' (dos comillas y en medio nada). 
+Otra versión puede ser con la función substr (para substraer), su versión mejorada nos permite:
+    TRY_CAST(REGEXP_SUBSTR(DISTANCE, '[0-9]*[.]*[0-9]') AS NUMBER(10, 2))
+Aquí le estamos diciendo que substraiga cualquier conjunto de dígitos entre el 0 y el 9 seguidos o no de un punto y seguidos o no de otro conjunto de dígitos entre 0 y 9.
+La diferencia entre el try_cast y el cast es que si hay un registro que no pueda convertir el cast falla y el try_cast lo convierte a nulo. No es mejor o peor uno u otro es cuestión de gustos
+pues si se usa bien no hay problema. Pero en cuanto a las expresiones regulares si te pueden ofrecer ciertas ventajas.
+Aunque correcta tu forma, es menos genérica.
+
+Legibilidad: Correcto
+
+Extra: Bien en redondear a dos decimales el resultado y limpiar los nulos por ceros para el runner 4. 
+
+*/
