948950cff901f37982e781c1fb3b275214d6007a
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2024-01-30 13:36:58 +0100

    Create dia2.sql

---
+++ Jose Perez/semana3/dia2.sql
@@ -0,0 +1,27 @@
+with create_colum_txn_type as(
+    select 
+        customer_id,
+        concat(split(txn_date, '-')[0],'-',split(txn_date, '-')[1]) as year_month,
+        decode(txn_type,'deposit',1,0) as deposit,
+        decode(txn_type,'purchase',1,0) as purchase,
+        decode(txn_type,'withdrawal',1,0) as withdrawal
+    from SQL_EN_LLAMAS.CASE03.CUSTOMER_TRANSACTIONS
+),
+total_txn_type as (
+    select 
+        customer_id, 
+        year_month
+    from 
+        create_colum_txn_type
+    group by 
+        customer_id, year_month
+    having 
+        (sum(deposit) > 1 and sum(purchase) > 1) or sum(withdrawal) > 1
+)  
+select
+    year_month,
+    count(*) as total_customers
+from 
+    total_txn_type
+group by 
+    year_month;
