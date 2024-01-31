18c4fc348d40fda4e50e36236c39b1beabccc371
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-01-31 10:47:42 +0100

    Update day_2.sql
    Aplicada la corrección

--- Daniel Jimenez/Semana-1/day_2.sql
+++ Daniel Jimenez/Semana-1/day_2.sql
@@ -1,9 +1,10 @@
 //¿Cuantos días ha visitado cada cliente el restaurante?
-SELECT  A.customer_id as id_cliente ,
-        COUNT(DISTINCT B.order_date) as dias_totales
-FROM    SQL_EN_LLAMAS.CASE01.MEMBERS A
-LEFT JOIN    SQL_EN_LLAMAS.CASE01.SALES B
-    ON A.customer_id = B.customer_id
+SELECT
+      A.customer_id as id_cliente
+    , COUNT(DISTINCT B.order_date) as dias_totales
+FROM SQL_EN_LLAMAS.CASE01.MEMBERS A
+    LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES B
+        ON A.customer_id = B.customer_id
 GROUP BY 1
 ORDER BY 2 DESC;
 
