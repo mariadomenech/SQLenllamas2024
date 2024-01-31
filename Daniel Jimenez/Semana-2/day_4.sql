4eebcbb87d6995b3a04e3d2391caa6625af61b25
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-01-31 10:46:19 +0100

    Update day_4.sql
    Aplicada la recomendación de código limpio

--- Daniel Jimenez/Semana-2/day_4.sql
+++ Daniel Jimenez/Semana-2/day_4.sql
@@ -6,6 +6,7 @@ WITH toppings_separados AS (
     FROM SQL_EN_LLAMAS.CASE02.PIZZA_RECIPES
         ,   LATERAL FLATTEN (input=>SPLIT(TOPPINGS, ',')) AS T /*He aprendido justo hoy a usar LATERAL FLATTEN, en este caso dividimos los toppings de cada receta en filas separadas. Luego SPLIT divide los toppings en una array y FLATTEN convierte esta array en filas. */
 )
+    
 SELECT  PIZZA_TOPPINGS.TOPPING_NAME AS nombre_topping
     ,   COUNT(toppings_separados.TOPPING_ID) AS numero_repeticiones
 FROM SQL_EN_LLAMAS.CASE02.PIZZA_TOPPINGS 
