86605a5c382f7f1cf558faf8a1f0a76a37b22f37
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-02-01 09:32:33 +0100

    Update Day1.sql

--- Paco Mantell/Week3/Day1.sql
+++ Paco Mantell/Week3/Day1.sql
@@ -1,32 +1,32 @@
 WITH CTE_CLEAN_DATA AS (
     /*Limpiamos la columna end_date, que contiene años erroneos, y obtenemos una columna de siguiente nodo*/
     SELECT customer_id,
-    region_id,
-    node_id,
-    start_date,
-    REPLACE(end_date,YEAR(end_date), YEAR(start_date)) end_date,
-    LEAD(node_id) IGNORE NULLS OVER (ORDER BY customer_id) next_node
+        region_id,
+        node_id,
+        start_date,
+        REPLACE(end_date,YEAR(end_date), YEAR(start_date)) end_date,
+        LEAD(node_id) IGNORE NULLS OVER (ORDER BY customer_id) next_node
     FROM sql_en_llamas.case03.customer_nodes
 ), CTE_DAYDIFF AS(
     /*seleccionamos solo las filas correspondientes a un cambio de nodo y calculamos la diferencia de dias*/
     SELECT customer_id,
-    DATEDIFF(DAY,start_date,end_date) daydiff
+        DATEDIFF(DAY,start_date,end_date) daydiff
     FROM CTE_CLEAN_DATA
     WHERE node_id!=next_node
     
 )
 /*Mostramos la media de dias por cliente y el total en la primera fila*/
 SELECT 'TOTAL' totals,
-CAST(AVG(daydiff) AS INT) avg_days
+    CAST(AVG(daydiff) AS INT) avg_days
 FROM CTE_DAYDIFF
 GROUP BY 1
 
 UNION ALL
 SELECT TO_VARCHAR(customer_id),
-avg_days
+    avg_days
 FROM(
     SELECT customer_id,
-    CAST(AVG(daydiff) AS INT) avg_days
+        CAST(AVG(daydiff) AS INT) avg_days
     FROM CTE_DAYDIFF A
     GROUP BY 1
     ORDER BY 1 ASC
