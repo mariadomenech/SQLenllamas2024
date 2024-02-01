3ab9a7cbd5ccffc304b804ce231c6a2a09db2cd2
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-02-01 13:41:51 +0100

    Update day_4.sql
    No tuve en cuenta que donde pone mes debía poner su nombre y no su número

--- Daniel Jimenez/Semana-3/day_4.sql
+++ Daniel Jimenez/Semana-3/day_4.sql
@@ -14,6 +14,7 @@ DECLARE
   total_depositado INTEGER;
   total_retiros INTEGER;
   balance INTEGER;
+  mes_nombre STRING;
 BEGIN
   SELECT COALESCE(SUM(TXN_AMOUNT), 0)-- He usado COALESCE a modo preventivo para tratar posibles NULLS, tras una revisión he comprobado que no hay casillas NULLS jeje 
   INTO total_depositado
@@ -46,9 +47,25 @@ BEGIN
   );
   
   balance := total_depositado - total_compras - total_retiros;
+
+    
+  SELECT CASE
+    WHEN :mes = 1 THEN 'ENERO'
+    WHEN :mes = 2 THEN 'FEBRERO'
+    WHEN :mes = 3 THEN 'MARZO'
+    WHEN :mes = 4 THEN 'ABRIL'
+    WHEN :mes = 5 THEN 'MAYO'
+    WHEN :mes = 6 THEN 'JUNIO'
+    WHEN :mes = 7 THEN 'JULIO'
+    WHEN :mes = 8 THEN 'AGOSTO'
+    WHEN :mes = 9 THEN 'SEPTIEMBRE'
+    WHEN :mes = 10 THEN 'OCTUBRE'
+    WHEN :mes = 11 THEN 'NOVIEMBRE'
+    WHEN :mes = 12 THEN 'DICIEMBRE'
+  END INTO mes_nombre;
   
-  RETURN 'El cliente ' || :CUSTOMER_ID || ' se ha gastado un total de ' || :total_compras || ' EUR en compras de productos en el mes ' || :mes || '. Su balance actual es de ' || :balance || ' EUR, habiendo depositado un total de ' || :total_depositado || ' EUR y retirado un total de ' || :total_retiros || ' EUR.';
+  RETURN 'El cliente ' || :CUSTOMER_ID || ' se ha gastado un total de ' || :total_compras || ' EUR en compras de productos en el mes de ' || :mes_nombre || '. Su balance actual es de ' || :balance || ' EUR, habiendo depositado un total de ' || :total_depositado || ' EUR y retirado un total de ' || :total_retiros || ' EUR.';
 END;
 
 
-CALL calcular_compras_daniel_jimenez_2(1,3); 
+CALL calcular_compras_daniel_jimenez_2(1,3);
