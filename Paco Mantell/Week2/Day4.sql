9275549709b5bd177c151e1efa879d8058cfee67
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-02-01 09:31:15 +0100

    Update Day4.sql

--- Paco Mantell/Week2/Day4.sql
+++ Paco Mantell/Week2/Day4.sql
@@ -1,13 +1,13 @@
 WITH CTE_SPLIT_ING AS (
     /* Desgloso los ingredientes por pizza y lo reuno en dos columnas*/
     SELECT pizza_id,
-    CAST(C.value AS INT) topping_id
+        CAST(C.value AS INT) topping_id
     FROM sql_en_llamas.case02.pizza_recipes,
     LATERAL FLATTEN(input=>SPLIT(toppings, ',')) C
 )
 /* Cuento las veces que aparece cada ingrediente*/
 SELECT topping_name,
-ZEROIFNULL(COUNT(B.topping_id)) times_used
+    ZEROIFNULL(COUNT(B.topping_id)) times_used
 FROM sql_en_llamas.case02.pizza_toppings A
 RIGHT JOIN CTE_SPLIT_ING B
     ON A.topping_id=B.topping_id
