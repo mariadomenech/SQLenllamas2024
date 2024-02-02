4fcf6b813365aa09b20224a7850d88ae3d25e388
AlexArcoCivica <156660711+AlexArcoCivica@users.noreply.github.com>
2024-02-02 10:13:55 +0100

    Update day_3.sql para añadir los meses en texto

--- Alejandro Arco/Semana3/day_3.sql
+++ Alejandro Arco/Semana3/day_3.sql
@@ -11,16 +11,31 @@ CREATE OR REPLACE PROCEDURE alejandroarco_gastos_cliente_mes (c_id INTEGER, mes 
 RETURNS VARCHAR
 LANGUAGE SQL
 AS
-DECLARE total INTEGER;
+DECLARE 
+    total INTEGER;
+    mes_texto VARCHAR(10);
 BEGIN
 
-    SELECT SUM(txn_amount) INTO total
+    SELECT SUM(txn_amount) INTO total --INTO total
     FROM customer_transactions
     WHERE customer_id = :c_id 
         AND MONTH(txn_date) = :mes
         AND txn_type = 'purchase';
+
+    -- Texto de los meses
+    SELECT CASE
+        WHEN :mes = 1 
+            THEN 'enero'
+        WHEN :mes = 2 
+            THEN 'febrero'
+        WHEN :mes = 3 
+            THEN 'marzo'
+        WHEN :mes = '4' 
+            THEN 'abril'
+        ELSE TO_VARCHAR(:mes)
+    END INTO mes_texto;
         
-RETURN 'El cliente ' || :c_id || ' se ha gastado un total de ' || total || ' euros en compras de productos en el mes de ' || :mes || '.';
+RETURN 'El cliente ' || :c_id || ' se ha gastado un total de ' || total || ' euros en compras de productos en el mes de ' || :mes_texto || '.';
 
 END;
 
