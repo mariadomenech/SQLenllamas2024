673001eb237956da43d5b2c5ef38da3f0f51229a
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-02-02 04:28:56 +0100

    Comentarios Juanpe 2.4
    Comentarios Juanpe 2.4

--- Maria Nieves Pérez/Semana_2/Reto_9.sql
+++ Maria Nieves Pérez/Semana_2/Reto_9.sql
@@ -12,3 +12,19 @@ FROM (
 INNER JOIN SQL_EN_LLAMAS.CASE02.PIZZA_TOPPINGS BB
     ON AA.toppings=BB.topping_id
 ORDER BY toppings;
+
+
+/*
+COMENTARIOS JUANPE: 
+
+RESULTADO: CORRECTO.
+
+CÓDIGO: CORRECTOS 
+
+LEGIBILIDAD: CORRECTA
+
+EXTRAS: raro pero original hacer el conteo con una función ventana yel distinct, lo normal en estos casos es montar el group by pero y asi 
+usasr un count normal y no hace falta distinct pero como te digo muy orignal ya que metes las funcinoes ventana
+
+PD: el reto 8 (semana 2 día 3) no lo has entregado, ¿se te ha pasado o no te salía? cualquier cosa no dudes en contactar
+*/
