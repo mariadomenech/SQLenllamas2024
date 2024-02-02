63190c75215a151f5e44de4aa7034dc3bcce3196
Pablo Nieto <153496982+panieto@users.noreply.github.com>
2024-02-02 21:24:06 +0100

    Update day3.sql

--- Pablo Nieto/Semana3/day3.sql
+++ Pablo Nieto/Semana3/day3.sql
@@ -16,7 +16,7 @@ BEGIN
         ELSE
             --Calculamos el importe total gastado en compras por el usuario indicado en el mes escogido.
             SELECT
-                NVL(SUM(txn_amount), 0) INTO :importe_compras
+                NVL(SUM(txn_amount), 0) INTO importe_compras
             FROM SQL_EN_LLAMAS.CASE03.CUSTOMER_TRANSACTIONS
             WHERE customer_id = :cliente
               AND EXTRACT(MONTH FROM txn_date) = :mes
@@ -25,7 +25,7 @@ BEGIN
             --Escribimos el mes correspondiente al número asociado insertado como parámetro de entrada.
             SELECT DECODE(:mes, 1, 'enero', 2, 'febrero', 3, 'marzo', 4, 'abril', 5, 'mayo', 6, 'junio', 7, 'julio', 8, 'agosto', 9, 'septiembre', 10, 'octubre', 11, 'noviembre', 12, 'diciembre') INTO mes_escrito;
         
-            RETURN ('El cliente ' || :cliente || ' se ha gastado un total de ' || :importe_compras || ' eur en compras de productos en el mes de ' || :mes_escrito || '.');
+            RETURN ('El cliente ' || :cliente || ' se ha gastado un total de ' || importe_compras || ' euros en compras de productos en el mes de ' || mes_escrito || '.');
     END;
 END;
 
