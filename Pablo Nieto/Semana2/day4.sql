08d2512b584bf65fae8fc6fe7e4fa0c44cd39401
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-02-02 04:23:13 +0100

    Comentarios Juanpe 2.4
    Comentarios Juanpe 2.4

--- Pablo Nieto/Semana2/day4.sql
+++ Pablo Nieto/Semana2/day4.sql
@@ -13,3 +13,16 @@ SELECT
     NVL(ft.frecuencia, 0) frecuencia
 FROM SQL_EN_LLAMAS.CASE02.PIZZA_TOPPINGS pt
 LEFT JOIN frecuencia_topping ft USING (topping_id)
+
+
+
+/*
+COMENTARIOS JUANPE: me gusta el SPLIT_TO_TABLE más que otras alternativas (que son igualemnte válidas) 
+
+RESULTADO: CORRECTO.
+
+CÓDIGO: CORRECTOS 
+
+LEGIBILIDAD: CORRECTA
+
+*/
