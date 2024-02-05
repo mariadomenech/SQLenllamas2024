ebf15a15edf4788d1174bbd5fcc26800fd6e6907
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-02-05 12:44:31 +0100

    Update day_1.sql
    He contemplado una CALL incorrecta y se me olvidó poner mi nombre en el PROCEDURE

--- Daniel Jimenez/Semana-4/day_1.sql
+++ Daniel Jimenez/Semana-4/day_1.sql
@@ -1,7 +1,7 @@
 /*Las tablas no están muy limpias, crea un procedimiento almacenado que informando el nombre de la tabla, nos devuelva el número de posibles duplicados*/
 
 
-CREATE OR REPLACE PROCEDURE contar_duplicados(tabla_nombre STRING)
+CREATE OR REPLACE PROCEDURE contar_duplicados_daniel_jimenez(tabla_nombre STRING)
 RETURNS STRING
 LANGUAGE SQL
 AS
@@ -11,11 +11,11 @@ DECLARE
 BEGIN
   CASE :tabla_nombre
     WHEN 'PRODUCT_DETAILS' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT PRODUCT_ID , PRICE , PRODUCT_NAME , CATEGORY_ID , SEGMENT_ID , STYLE_ID , CATEGORY_NAME , SEGMENT_NAME , STYLE_NAME) INTO total_duplicados FROM PRODUCT_DETAILS; -- Al restar el número total de filas únicas del numero total de filas obtenemos el número de filas que están duplicadas.
+      SELECT COUNT(*) - COUNT(DISTINCT PRODUCT_ID , PRICE , PRODUCT_NAME , CATEGORY_ID , SEGMENT_ID , STYLE_ID , CATEGORY_NAME , SEGMENT_NAME , STYLE_NAME) INTO total_duplicados FROM PRODUCT_DETAILS;
     WHEN 'PRODUCT_HIERARCHY' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT ID , PARENT_ID , LEVEL_TEXT , LEVEL_NAME) INTO total_duplicados FROM PRODUCT_HIERARCHY;
+      SELECT COUNT(*) - COUNT(DISTINCT PARENT_ID , LEVEL_TEXT , LEVEL_NAME) INTO total_duplicados FROM PRODUCT_HIERARCHY;
     WHEN 'PRODUCT_PRICES' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT ID , PRODUCT_ID , PRICE) INTO total_duplicados FROM PRODUCT_PRICES;
+      SELECT COUNT(*) - COUNT(DISTINCT PRODUCT_ID , PRICE) INTO total_duplicados FROM PRODUCT_PRICES;
     WHEN 'SALES' THEN
       SELECT COUNT(*) - COUNT(DISTINCT PROD_ID , QTY , PRICE , DISCOUNT , MEMBER , TXN_ID , START_TXN_TIME) INTO total_duplicados FROM SALES;
     ELSE
@@ -25,7 +25,8 @@ BEGIN
   RETURN 'La tabla ' || :tabla_nombre || ' tiene ' || total_duplicados || ' filas duplicadas.';
 END;
 
-CALL contar_duplicados ('SALES');
-CALL contar_duplicados ('PRODUCT_PRICES');
-CALL contar_duplicados ('PRODUCT_HIERARCHY');
-CALL contar_duplicados ('PRODUCT_DETAILS');
+
+CALL contar_duplicados_daniel_jimenez ('SALES');
+CALL contar_duplicados_daniel_jimenez ('PRODUCT_PRICES');
+CALL contar_duplicados_daniel_jimenez ('PRODUCT_HIERARCHY');
+CALL contar_duplicados_daniel_jimenez ('PRODUCT_DETAILS');
