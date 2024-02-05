dc60344bda936f8a2ebce8c77fa95fb0324c9c52
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-02-05 19:51:43 +0100

    Comentarios Juanpe 3.1
    Comentarios Juanpe 3.1

--- Manu Reina/SEMANA3/Day1.sql
+++ Manu Reina/SEMANA3/Day1.sql
@@ -40,3 +40,22 @@ FROM (
                 WHEN NODO_SIGUIENTE <> NODE_ID THEN END_DATE
             END AS FECHA_FINALIZACION_ACTUALIZADA
         FROM CLEANED_NODES)A;
+/*
+COMENTARIOS JUANPE:
+
+RESULTADO: INCORRECTO! CASI!! El reultado correcto es 17.865859 te explico en el siguiente apartado la diferencia.
+
+CODIGO: INCORRECTO! La lógica del ejercicio es tal cual la planteas salvo por un pequeño matiz, haces demasiado pronto el filtro: END_DATE <> TO_DATE('9999-12-31')
+Este filtro debiera ser justo al final antes de tu punto y coma. Y por consiguiente debería ser: FECHA_FINALIZACION_ACTUALIZADA <> TO_DATE('9999-12-31')
+¿Qué ocurre de una forma y de otra? Veamos el ejemplo del customer_id 1.
+Primero estuvo en nodo 4, durante 12 días y pasó al 2, que estuvo 1 día y pasó al 5 que estuvo 11 días y pasó al nodo 3 que estuvo 20 días y pasó al 2.
+Este es el último hasta la fecha. Por tanto (12+1+11+20)/4=44/4=11 días. Eso es lo que sabemos si miramos a ojo pero, ¿al poner el filtro antes qué ocurre?
+El ulitmo nodo en que está es el 2, pero hay doble registro. En el primer registro estuvo 26 días y luego hubo otro registro pero sin cambiar de nodo por lo tanto
+no debemos tener ese 26 en la media, y al filtrar antes de tiempo por la fecha abierta '9999-12-31' te ocurre esta diferencia. Es decir, has filtrado el último 
+registro pero no el último nodo entero. Si esto sigue sin cuadrarte podemos verlo más en detalle, no dudes en preguntarme.
+
+LEGIBLIDAD: CORRECTO
+
+EXTRA: Muy bien resuelto salvo por el matiz bien uso de la lógica y bien jugado con los nodos anteriores y posteriores para establecer las fechas reales de cambio
+de nodo.
+*/
