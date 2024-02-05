a3576766db1cafa49384937c98634d433c584b4b
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-02-05 15:46:42 +0100

    Update day_1.sql
    He tocado el conteo de duplicados, creo que ahora está mejor

--- Daniel Jimenez/Semana-4/day_1.sql
+++ Daniel Jimenez/Semana-4/day_1.sql
@@ -1,7 +1,7 @@
 /*Las tablas no están muy limpias, crea un procedimiento almacenado que informando el nombre de la tabla, nos devuelva el número de posibles duplicados*/
 
 
-CREATE OR REPLACE PROCEDURE contar_duplicados_daniel_jimenez(tabla_nombre STRING)
+CREATE OR REPLACE PROCEDURE contar_duplicados_daniel_jimenez2(tabla_nombre STRING)
 RETURNS STRING
 LANGUAGE SQL
 AS
@@ -11,22 +11,50 @@ DECLARE
 BEGIN
   CASE :tabla_nombre
     WHEN 'PRODUCT_DETAILS' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT PRODUCT_ID , PRICE , PRODUCT_NAME , CATEGORY_ID , SEGMENT_ID , STYLE_ID , CATEGORY_NAME , SEGMENT_NAME , STYLE_NAME) INTO total_duplicados FROM PRODUCT_DETAILS;
+      SELECT COUNT(*)
+      INTO total_duplicados
+      FROM (
+        SELECT PRODUCT_ID, PRICE, PRODUCT_NAME, CATEGORY_ID, SEGMENT_ID, STYLE_ID, CATEGORY_NAME, SEGMENT_NAME, STYLE_NAME
+        FROM PRODUCT_DETAILS
+        GROUP BY PRODUCT_ID, PRICE, PRODUCT_NAME, CATEGORY_ID, SEGMENT_ID, STYLE_ID, CATEGORY_NAME, SEGMENT_NAME, STYLE_NAME
+        HAVING COUNT(PRODUCT_ID) > 1
+      );
     WHEN 'PRODUCT_HIERARCHY' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT PARENT_ID , LEVEL_TEXT , LEVEL_NAME) INTO total_duplicados FROM PRODUCT_HIERARCHY;
+      SELECT COUNT(*)
+      INTO total_duplicados
+      FROM (
+        SELECT PARENT_ID, LEVEL_TEXT, LEVEL_NAME
+        FROM PRODUCT_HIERARCHY
+        GROUP BY PARENT_ID, LEVEL_TEXT, LEVEL_NAME
+        HAVING COUNT(PARENT_ID) > 1
+      );
     WHEN 'PRODUCT_PRICES' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT PRODUCT_ID , PRICE) INTO total_duplicados FROM PRODUCT_PRICES;
+      SELECT COUNT(*)
+      INTO total_duplicados
+      FROM (
+        SELECT PRODUCT_ID, PRICE
+        FROM PRODUCT_PRICES
+        GROUP BY PRODUCT_ID, PRICE
+        HAVING COUNT(PRODUCT_ID) > 1
+      );
     WHEN 'SALES' THEN
-      SELECT COUNT(*) - COUNT(DISTINCT PROD_ID , QTY , PRICE , DISCOUNT , MEMBER , TXN_ID , START_TXN_TIME) INTO total_duplicados FROM SALES;
+      SELECT COUNT(*)
+      INTO total_duplicados
+      FROM (
+        SELECT PROD_ID, QTY, PRICE, DISCOUNT, MEMBER, TXN_ID, START_TXN_TIME
+        FROM SALES
+        GROUP BY PROD_ID, QTY, PRICE, DISCOUNT, MEMBER, TXN_ID, START_TXN_TIME
+        HAVING COUNT(PROD_ID) > 1
+      );
     ELSE
-      RETURN 'La tabla ' || :tabla_nombre || ' no es válida.';
+      RETURN 'La tabla ' || tabla_nombre || ' no es válida.';
   END CASE;
 
-  RETURN 'La tabla ' || :tabla_nombre || ' tiene ' || total_duplicados || ' filas duplicadas.';
+  RETURN 'La tabla ' || tabla_nombre || ' tiene ' || total_duplicados || ' filas duplicadas.';
 END;
+/*Después de pensar que lo había terminado me puse a revisar tablas y creo que ahora SÍ cuenta bien los duplicados, en la versión anterior en la tabla HIERARCHY me daba 2 duplicados*/
 
-
-CALL contar_duplicados_daniel_jimenez ('SALES');
-CALL contar_duplicados_daniel_jimenez ('PRODUCT_PRICES');
-CALL contar_duplicados_daniel_jimenez ('PRODUCT_HIERARCHY');
-CALL contar_duplicados_daniel_jimenez ('PRODUCT_DETAILS');
+CALL contar_duplicados_daniel_jimenez2 ('SALES');
+CALL contar_duplicados_daniel_jimenez2 ('PRODUCT_PRICES');
+CALL contar_duplicados_daniel_jimenez2 ('PRODUCT_HIERARCHY');
+CALL contar_duplicados_daniel_jimenez2 ('PRODUCT_DETAILS');
