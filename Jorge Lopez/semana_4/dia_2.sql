edd79ee92716bf766fc170d8f84dfbde1bd7ea88
Yorch <62995534+YorchGR@users.noreply.github.com>
2024-02-07 10:33:59 +0100

    Solución dia 2

---
+++ Jorge Lopez/semana_4/dia_2.sql
@@ -0,0 +1,26 @@
+/*Día 2: ¿Cuál es la combinación de productos distintos más repetida por cada compra? Mínimo tres productos por compra.
+
+No tengo mucho que explicar. Me has ido enseñando y ayudando varios días durante todo el proceso.*/
+
+SELECT *
+FROM 
+    (SELECT
+        LISTADO,
+        COUNT(*) AS PROD_REP,
+        DENSE_RANK() OVER (ORDER BY PROD_REP DESC) AS RANKING
+    FROM 
+        (SELECT
+            S.TXN_ID,
+            COUNT(DISTINCT S.PROD_ID) AS DIFF_PRODUCT,
+            LISTAGG(DISTINCT P.PRODUCT_NAME, ' || ') WITHIN GROUP (ORDER BY P.PRODUCT_NAME) AS LISTADO
+        FROM
+            CASE04.SALES AS S
+            RIGHT JOIN CASE04.PRODUCT_DETAILS AS P ON S.PROD_ID = P.PRODUCT_ID
+        GROUP BY
+            S.TXN_ID
+        HAVING
+            DIFF_PRODUCT >= 3)
+    GROUP BY
+        LISTADO)
+WHERE
+    RANKING = 1;
