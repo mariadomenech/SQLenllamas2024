d2bec76294a5199d4711f81c0c8598148a8e3c4e
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-02-08 19:09:29 +0100

    Corrección Week02_Day02.sql

--- Jose Manuel Bermúdez/week02/day02/Week02_Day02.sql
+++ Jose Manuel Bermúdez/week02/day02/Week02_Day02.sql
@@ -7,3 +7,15 @@ SELECT runner_id AS "Runner",
 			(SUM(CASE WHEN regexp_replace(duration, '[^0-9.]', '') <> '' THEN CAST(regexp_replace(duration, '[^0-9.]', '') AS DECIMAL(15,2)) ELSE 0 END) / 60) AS "Velocidad Promedio (Km/h)"
 FROM runner_orders
 GROUP BY runner_id;
+
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+
+No es del todo correcto el código, se pedía velocidad promedio. Una vez que calculas los km/hora de cada pedido, haz la media por runner.
+Me gusta que hayas usado expresiones regulares para limpiar los campos, también podías haber usado: REGEXP_REPLACE(DISTANCE, '[a-zA-Z]', '').
+
+*/
