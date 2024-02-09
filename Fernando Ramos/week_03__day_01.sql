8c053dd00a25b97f93f1d44a9b71d3cbd36c09c8
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-02-09 09:51:03 +0100

    Corrección week_03__day_01.sql

--- Fernando Ramos/week_03__day_01.sql
+++ Fernando Ramos/week_03__day_01.sql
@@ -15,3 +15,45 @@ from nodos_previo_proximo
 --- quitamos cuando el cambio es al mismo nodo ---
 where 
     node_id <> nodo_proximo; 
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+
+La lógica del ejercicio es tal cual la planteas empieza bien, pero, pasas por alto una cosilla que hace que el resultado no sea correcto:
+ - Cuando un nodo está varios registros solo cuenta uno de los registros en vez del primero al ultimo
+
+RESULTADO CORRECTO: 17.865859.
+
+CÓDIGO: te explico con un ejemplo. Cojemos el customer_id = 1:
+-------------------------------------------------
+NODO    START        END           D1     D2
+-------------------------------------------------
+4	    02/01/2020    03/01/2020	1
+                                          12
+4	    04/01/2020    14/01/2020	10	
+-------------------------------------------------
+2	    15/01/2020    16/01/2020	1	  1
+-------------------------------------------------
+5	    17/01/2020    28/01/2020	11	  11
+-------------------------------------------------
+3	    29/01/2020    18/02/2020	20	  20
+-------------------------------------------------
+2	    19/02/2020    16/03/2020	26	
+                                          26+X+1
+2	    17/03/2020    31/12/9999	X	
+-------------------------------------------------
+D1 sería la diferencia de días por cada registro, y D2 es la diferencia de días hasta que cambia el nodo. Tú código está haciendo esto:
+(10+1+11+20)/4=42/4=10.5
+¿Que sería lo correcto?:
+(12+1+11+20)/4=44/4=11
+
+Te animo a que lo rehagas de nuevo teniendo esto en cuenta, puedes hacer la prueba con algunos concretos por ejemplo customer (1,24,62,447),
+ver a mano que debe de salir y ver si te sale.
+
+A parte del lead también existe el lag que en vez del siguiente te mira el anterior, te lo digo porque puede ayudarte a identificar las casuisticas
+a tener en cuenta para resolverlo.
+
+*/
