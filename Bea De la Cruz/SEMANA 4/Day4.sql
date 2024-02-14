f8d7aa100c1fbb9b7ca7aec32ab6ca368d66ba24
Beacruzmo <156338870+Beacruzmo@users.noreply.github.com>
2024-02-14 13:19:09 +0100

    Add files via upload

---
+++ Bea De la Cruz/SEMANA 4/Day4.sql
@@ -0,0 +1,16 @@
+
+with id_imp as (
+    select 
+        txn_id,
+        sum(price*(1-discount/100)*qty) as imp
+    from (
+        select distinct *
+        from sales
+        )
+    group by txn_id
+)
+select 
+    percentile_cont(0.25) within group (order by imp) as perc_25,
+    percentile_cont(0.50) within group (order by imp) as perc_50,
+    percentile_cont(0.75) within group (order by imp) as perc_75
+from id_imp;
