4c39af5cc162cf86fbf7cff13b4a8f4f246d55e5
AlexArcoCivica <156660711+AlexArcoCivica@users.noreply.github.com>
2024-02-14 14:57:56 +0100

    Update day_4.sql

--- Alejandro Arco/Semana4/day_4.sql
+++ Alejandro Arco/Semana4/day_4.sql
@@ -8,7 +8,8 @@ USE SCHEMA CASE04;
 WITH importe_transaccion AS (
     SELECT --distinct si hubiera duplicados
         txn_id,
-        SUM(qty * price - discount) AS total_transaccion  -- He puesto el descuento como cantidad, si fuera un porcentaje tendría que cambiar la fórmula para calcular este total
+        --SUM(qty * price - discount) AS total_transaccion -- Con el descuento como cantidad
+        SUM(price*qty*(1-discount/100)) AS total_transaccion -- Con el descuento como porcentaje
     FROM sales
     GROUP BY txn_id
 )
