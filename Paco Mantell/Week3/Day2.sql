0367c1b503b81055153e110722fee676f54e3c6d
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-02-19 08:01:04 +0100

    Comentarios Juanpe 3.2
    Comentarios Juanpe 3.2

--- Paco Mantell/Week3/Day2.sql
+++ Paco Mantell/Week3/Day2.sql
@@ -13,3 +13,28 @@ SELECT txn_month "month",
 FROM CTE_COUNT_TXN_TYPE
 WHERE cant_type_txn > 1
 GROUP BY 1
+
+
+/*COMENTARIOS JUANPE
+
+RESULTADO: INCORRECTO
+
+CÓDIGO: INCORRECTO. El problema está en que lo que pedía el ejercicio era: contar aquellos que cumplant (DEPOSITO > 1 AND COMPRA > 1) OR RETIRO > 1
+
+La solución era:
+SELECT ANYO
+     , MES
+     , COUNT(CUSTOMER_ID) AS CLIENTES
+FROM (SELECT CUSTOMER_ID
+           , EXTRACT(YEAR FROM TXN_DATE) AS ANYO
+           , EXTRACT(MONTH FROM TXN_DATE) AS MES
+           , SUM(DECODE(TXN_TYPE,'deposit',1,0))    AS DEPOSITO
+           , SUM(DECODE(TXN_TYPE,'purchase',1,0))   AS COMPRA
+           , SUM(DECODE(TXN_TYPE,'withdrawal',1,0)) AS RETIRO
+       FROM CUSTOMER_TRANSACTIONS
+       GROUP BY ANYO, MES, CUSTOMER_ID
+      ) A
+WHERE (DEPOSITO > 1 AND COMPRA > 1) OR RETIRO > 1
+GROUP BY ANYO, MES
+ORDER BY ANYO, MES;
+*/
