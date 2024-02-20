aa07ddf560aee23064f5d0e447c2d03d737285ad
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-02-20 19:10:49 +0100

    CorrecciónWeek04_Day04.sql

--- Jose Manuel Bermúdez/week04/day04/Week04_Day04.sql
+++ Jose Manuel Bermúdez/week04/day04/Week04_Day04.sql
@@ -5,3 +5,17 @@ SELECT DISTINCT PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY ((qty * price) - ((
 				PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ((qty * price) - ((qty * price * discount) / 100))) AS "Percentil 50",
 				PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY ((qty * price) - ((qty * price * discount) / 100))) AS "Percentil 75"
 FROM sales;
+
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+Tal y como lo has planetado has sacado los distintos percentiles por producto.
+
+Para que sea por transacción como pedimos en el enunciado, debes calcular los ingresos por transacción previamente. 
+Y después calcular los percentiles sobre esos importes.
+
+La función que coges es la correcta!
+*/
