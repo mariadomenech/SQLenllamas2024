091d2878c11c9cea62273604f37dcd1caa7be59c
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-02-20 18:41:26 +0100

    Corrección Week04_Day02.sql

--- Jose Manuel Bermúdez/week04/day02/Week04_Day02.sql
+++ Jose Manuel Bermúdez/week04/day02/Week04_Day02.sql
@@ -26,3 +26,28 @@ JOIN product_details pd
 		gmrc.product2 = pd.product_id OR
 		gmrc.product3 = pd.product_id
 GROUP BY gmrc.num_ocur;
+
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+/* 
+
+Creo que la interpretación de la pregunta no ha quedado del todo clara. No se preguntaba cuál era la combinación de 3 productos más repetida, sino qué combinación (transacción completa)
+de al menos 3 producto es la que más se repite.
+
+La combinación más repetida es, un total de 5 veces:
+-Black Straight Jeans - Womens
+- Cream Relaxed Jeans - Womens
+- Grey Fashion Jacket - Womens
+- Navy Oversized Jeans - Womens
+- Navy Solid Socks - Mens
+- Pink Fluro Polkadot Socks - Mens
+- Teal Button Up Shirt - Mens
+- White Tee Shirt - Mens
+
+
+Por otro lado, ten cuidado con el uso del LIMIT para sacar un TOP1, en caso de empate, solo sacas una combianción.
+
+*/
