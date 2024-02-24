15ce7bd31490d68b877ad8d4abe325592ee05052
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-02-24 10:56:03 +0100

    Corrección Day4.sql

--- Bea De la Cruz/SEMANA 4/Day4.sql
+++ Bea De la Cruz/SEMANA 4/Day4.sql
@@ -14,3 +14,12 @@ select
     percentile_cont(0.50) within group (order by imp) as perc_50,
     percentile_cont(0.75) within group (order by imp) as perc_75
 from id_imp;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto.
+
+*/
