696707e4bfc9af36606e41383667bc90804c3753
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-02-24 10:50:36 +0100

    Corrección day_4.sql

--- Alejandro Arco/Semana4/day_4.sql
+++ Alejandro Arco/Semana4/day_4.sql
@@ -20,5 +20,33 @@ SELECT
     PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY total_transaccion) AS percentil_75
 FROM importe_transaccion;
 
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
 
+El resultado no es correcto.
 
+He visto que has probado a utilizar distinct para quitar los duplicados pero eso tendrias que hacerlo en un paso previo sobre la tabla sales en su totalidad, y no es la CTE en la que calculas el total
+de la transacción:
+
+WITH CLEAN_SALES AS (
+    SELECT DISTINCT * --AQUI QUITAMOS LOS DUPLICADOS
+    FROM SALES
+),
+
+IMPORTE_TRANSACCION AS (
+    SELECT
+          TXN_ID
+        , SUM(PRICE*QTY*(100-DISCOUNT)/100) AS TOTAL_TRANSACCION
+    FROM CLEAN_SALES
+    GROUP BY TXN_ID
+)
+
+SELECT
+      PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY TOTAL_TRANSACCION) AS PERCENTIL_25
+    , PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY TOTAL_TRANSACCION) AS PERCENTIL_50
+    , PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY TOTAL_TRANSACCION) AS PERCENTIL_75
+FROM IMPORTE_TRANSACCION;
+
+*/
