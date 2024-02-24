9c13c9210258aefee1ca1d466d2b89f6caf4d19c
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-02-24 11:08:48 +0100

    Corrección day4_week4.sql

--- Bea Exposito/day4_week4.sql
+++ Bea Exposito/day4_week4.sql
@@ -8,3 +8,26 @@ FROM(
     SELECT ( qty * price) * (1 - (discount/100)) as income
     FROM (SELECT DISTINCT * FROM case04.sales )
      );
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado no es correcto.
+
+a la hora de hacer los calculos deberias hacerlo por transacción como se pide:
+
+SELECT
+      PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY income) :: float AS incomes_percentil_25
+    , PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY income) :: float AS incomes_percentil_50
+    , PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY income) :: float AS incomes_percentil_75
+FROM (
+    SELECT
+          txn_id
+        , sum(qty*price*(100-discount)/100) as income
+    FROM (SELECT DISTINCT * FROM case04.sales )
+    GROUP BY txn_id
+     );
+
+*/
